f6e6985857ff926320d2ad089b03481774874db8
Luke Holder <lukemh@gmail.com>
2015-02-19 16:53:10 +0500

    test seeder bootstrap

---
+++ plugins/market/Market/Seed/Market_TestSeeder.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Market\Seed;
+
+/**
+ * Test Data useful during development
+ */
+class Market_TestSeeder implements Market_SeederInterface
+{
+    public function seed()
+    {
+        // TODO: Implement seed() method.
+    }
+}
