5049588ddb1dd34e9b3715d45545f2d1a00d2ed1
Luke Holder <lukemh@gmail.com>
2015-02-19 10:42:56 +0500

    order type and product type handles are unique

--- src/records/Market_ProductTypeRecord.php
+++ src/records/Market_ProductTypeRecord.php
@@ -16,8 +16,6 @@ class Market_ProductTypeRecord extends BaseRecord
 {
 
 	/**
-	 * @inheritDoc BaseRecord::getTableName()
-	 *
 	 * @return string
 	 */
 	public function getTableName()
@@ -25,34 +23,35 @@ class Market_ProductTypeRecord extends BaseRecord
 		return 'market_producttypes';
 	}
 
+    /**
+     * @return array
+     */
+    public function defineIndexes()
+    {
+        return [
+            ['columns' => ['handle'], 'unique' => true],
+        ];
+    }
+
 	/**
-	 * @inheritDoc BaseRecord::defineRelations()
-	 *
 	 * @return array
 	 */
 	public function defineRelations()
 	{
-		return array(
-			'fieldLayout' => array(static::BELONGS_TO, 'FieldLayoutRecord', 'onDelete' => static::SET_NULL),
-		);
+		return [
+			'fieldLayout' => [static::BELONGS_TO, 'FieldLayoutRecord', 'onDelete' => static::SET_NULL],
+		];
 	}
 
-
-	// Protected Methods
-	// =========================================================================
-
 	/**
-	 * @inheritDoc BaseRecord::defineAttributes()
-	 *
 	 * @return array
 	 */
 	protected function defineAttributes()
 	{
-		return array(
-			'name'          => array(AttributeType::Name, 'required' => true),
-			'handle'        => array(AttributeType::Handle, 'required' => true),
-			'fieldLayoutId' => AttributeType::Number,
-		);
+		return [
+			'name'   => [AttributeType::Name, 'required' => true],
+			'handle' => [AttributeType::Handle, 'required' => true],
+		];
 	}
 
 }
