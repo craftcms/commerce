d8ae004dd46f255d322a9c51a4aae58636dba827
Luke Holder <lukemh@gmail.com>
2015-02-27 15:42:52 +0500

    payment processing. displying transactions

--- plugins/market/controllers/Market_CartPaymentController.php
+++ plugins/market/controllers/Market_CartPaymentController.php
@@ -54,11 +54,35 @@ class Market_CartPaymentController extends Market_BaseController
         $paymentForm = new Market_PaymentFormModel;
         $paymentForm->attributes = $_POST;
 
-        if(craft()->market_cart->pay($paymentForm)) {
-            craft()->userSession->setFlash('market', 'Successful payment');
-            $this->redirectToPostedUrl();
+        //in case of success "pay" redirects us somewhere
+        if(!craft()->market_payment->processPayment($paymentForm, $customError)) {
+            craft()->urlManager->setRouteVariables(compact('paymentForm', 'customError'));
+        }
+    }
+
+    public function actionCancel()
+    {
+        $this->actionGoToComplete();
+        $this->redirect('market/cart');
+    }
+
+    public function actionSuccess()
+    {
+        $this->actionGoToComplete();
+        $this->redirect('market/cart');
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function actionGoToComplete()
+    {
+        $order = craft()->market_cart->getCart();
+
+        if($order->canTransit(Market_OrderRecord::STATE_COMPLETE)) {
+            $order->transition(Market_OrderRecord::STATE_COMPLETE);
         } else {
-            craft()->urlManager->setRouteVariables(['paymentForm' => $paymentForm]);
+            throw new Exception('unable to go to payment state from the state: ' . $order->state);
         }
     }
 }
