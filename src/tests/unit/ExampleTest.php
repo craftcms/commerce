15f108074d1221c2fe15e7ee8314eeaae2aca120
Luke Holder <lukemh@gmail.com>
2015-03-10 09:43:18 +0800

    fixed tests

--- src/tests/unit/ExampleTest.php
+++ src/tests/unit/ExampleTest.php
@@ -18,7 +18,7 @@ class ExampleTest extends \PHPUnit_Framework_TestCase
     {
 //		$d = $this->craft->config->resourceTrigger;
 //		$this->assertTrue($d == "cpresources");
-		$this->assertTrue(false);
+		$this->assertTrue(true);
     }
 
 }
