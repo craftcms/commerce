8fe51aa2543937c9ab17720a9f31a5750adb411c
Luke Holder <lukemh@gmail.com>
2015-04-15 17:20:48 +0500

    #60 Add return url and cancel url

--- src/controllers/Market_CartPaymentController.php
+++ src/controllers/Market_CartPaymentController.php
@@ -20,9 +20,10 @@ class Market_CartPaymentController extends Market_BaseController
 		$this->requirePostRequest();
 
 		$id = craft()->request->getPost('shippingMethodId');
-		
+        $orderTypeHandle         = craft()->request->getPost('orderTypeHandle');
+        $cart                    = craft()->market_cart->getCart($orderTypeHandle);
 
-		if (craft()->market_cart->setShippingMethod($id)) {
+		if (craft()->market_cart->setShippingMethod($cart, $id)) {
 			craft()->userSession->setFlash('market', 'Shipping method has been set');
 			$this->redirectToPostedUrl();
 		} else {
@@ -39,37 +40,41 @@ class Market_CartPaymentController extends Market_BaseController
 
         $paymentForm             = new Market_PaymentFormModel;
         $paymentForm->attributes = $_POST;
+        $redirect                = craft()->request->getPost('redirect');
         $orderTypeHandle         = craft()->request->getPost('orderTypeHandle');
         $cart                    = craft()->market_cart->getCart($orderTypeHandle);
 
 		//in case of success "pay" redirects us somewhere
-		if (!craft()->market_payment->processPayment($cart, $paymentForm, $customError)) {
+		if (!craft()->market_payment->processPayment($cart, $paymentForm, $redirect, $customError)) {
 			craft()->urlManager->setRouteVariables(compact('paymentForm', 'customError'));
 		}
 	}
 
-	public function actionCancel()
-	{
-		$this->actionGoToComplete();
-	}
+    /**
+     * Complete order
+     *
+     * @throws Exception
+     * @throws HttpException
+     */
+    public function actionComplete()
+    {
+        $id = craft()->request->getParam('id');
+        $transaction = craft()->market_transaction->getById($id);
 
-	/**
-	 * @throws Exception
-	 */
-	public function actionGoToComplete()
-	{
-		$orderTypeHandle = craft()->request->getPost('orderTypeHandle');
-		$order = craft()->market_cart->getCart($orderTypeHandle);
+        if(!$transaction->id) {
+            throw new HttpException(400);
+        }
 
-		if ($order->canTransit(Market_OrderRecord::STATE_COMPLETE)) {
-			craft()->market_order->complete($order);
-		} else {
-			throw new Exception('unable to go to payment state from the state: ' . $order->state);
-		}
-	}
+        $order = $transaction->order;
 
-	public function actionSuccess()
-	{
-		$this->actionGoToComplete();
-	}
+        if($order->state != Market_OrderRecord::STATE_COMPLETE) {
+            if ($order->canTransit(Market_OrderRecord::STATE_COMPLETE)) {
+                craft()->market_order->complete($order);
+            } else {
+                throw new Exception('unable to go to complete state from the state: ' . $order->state);
+            }
+        }
+
+        $this->redirect('market/orders');
+    }
 }
