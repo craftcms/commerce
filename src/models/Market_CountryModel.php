39d3fcbcd85173ce9fe5a04cd4db5b0f35ad347e
Luke Holder <lukemh@gmail.com>
2015-04-19 17:18:34 +0500

    Tax Adjuster takes qty into account. Some UI fixes

--- src/models/Market_CountryModel.php
+++ src/models/Market_CountryModel.php
@@ -13,12 +13,26 @@ namespace Craft;
  */
 class Market_CountryModel extends BaseModel
 {
+    /**
+     * @return string
+     */
 	public function getCpEditUrl()
 	{
 		return UrlHelper::getCpUrl('market/settings/countries/' . $this->id);
 	}
 
-	protected function defineAttributes()
+    /**
+     * @return string
+     */
+    function __toString()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return array
+     */
+    protected function defineAttributes()
 	{
 		return [
 			'id'            => AttributeType::Number,
