01152afac4e08e1b34b2ebff8470b6f9067fd7c8
Luke Holder <lukemh@gmail.com>
2015-04-20 00:47:07 +0500

    #64 sending emails on status change

--- src/MarketPlugin.php
+++ src/MarketPlugin.php
@@ -17,6 +17,8 @@ class MarketPlugin extends BasePlugin
 
 	function init()
 	{
+        //init global event handler
+        craft()->on('market_orderHistory.onStatusChange', [craft()->market_orderStatus, 'statusChangeHandler']);
 	}
 
 	public function getName()
