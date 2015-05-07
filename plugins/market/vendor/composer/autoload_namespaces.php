57b3764e058eba8d60cd9eec376d4e3b029b15d4
Luke Holder <lukemh@gmail.com>
2015-05-07 15:45:19 -0700

    Blowing deps away.

--- plugins/market/vendor/composer/autoload_namespaces.php
+++ plugins/market/vendor/composer/autoload_namespaces.php
@@ -6,8 +6,4 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Pimple' => array($vendorDir . '/pimple/pimple/src'),
-    'Omnipay\\Common\\' => array($vendorDir . '/omnipay/common/src'),
-    'Guzzle\\Tests' => array($vendorDir . '/guzzle/guzzle/tests'),
-    'Guzzle' => array($vendorDir . '/guzzle/guzzle/src'),
 );
