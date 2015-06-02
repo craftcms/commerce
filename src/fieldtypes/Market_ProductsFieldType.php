112b1845236e7b05d2be9e61656aa9dbe4fc316b
Luke Holder <lukemh@gmail.com>
2015-06-02 10:45:57 +0800

    Improvement. Products Fieldtype added

---
+++ src/fieldtypes/Market_ProductsFieldType.php
@@ -0,0 +1,28 @@
+<?php
+namespace Craft;
+
+class Market_ProductsFieldType extends BaseElementFieldType
+{
+	// Properties
+	// =========================================================================
+
+	/**
+	 * The element type this field deals with.
+	 *
+	 * @var string $elementType
+	 */
+	protected $elementType = 'Market_Product';
+
+	// Protected Methods
+	// =========================================================================
+
+	/**
+	 * @inheritDoc BaseElementFieldType::getAddButtonLabel()
+	 *
+	 * @return string
+	 */
+	protected function getAddButtonLabel()
+	{
+		return Craft::t('Add an product');
+	}
+}
