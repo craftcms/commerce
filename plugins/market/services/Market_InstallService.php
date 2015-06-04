aa044c5f97c2f8543e89d339e52729d7139bab90
Luke Holder <lukemh@gmail.com>
2015-06-04 19:33:25 +0800

    Install Service Class created.

---
+++ plugins/market/services/Market_InstallService.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Craft;
+/**
+ * Class Market_InstallService
+ *
+ * @package Craft
+ */
+class Market_InstallService extends BaseApplicationComponent{
+
+	public function install(){
+
+		$this->_createTables();
+		$this->_addForeignKeys();
+	}
+
+	private function _createTables()
+	{
+	}
+
+	private function _addForeignKeys()
+	{
+	}
+}
