fd27e30c750e56f7aa9d58641715f6b8175f1822
Luke Holder <lukemh@gmail.com>
2015-06-16 11:40:27 +0800

    Temp commit - Half Migration completed.

---
+++ src/services/Market_PurchasableService.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Craft;
+
+/**
+ * Class Market_PurchasableService
+ *
+ * @package Craft
+ */
+class Market_PurchasableService extends BaseApplicationComponent
+{
+
+	public function savePurchasableElement(BaseElementModel $model)
+	{
+		if(craft()->elements->saveElement($model)) {
+
+		}
+	}
+}
