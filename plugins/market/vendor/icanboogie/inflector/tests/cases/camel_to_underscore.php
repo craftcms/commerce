edfe953ee466d72e46f57845ee485405aaf0e3a2
Benjamin David <ben@dukt.net>
2015-07-15 15:08:39 +0200

    Removed vendor directory

--- plugins/market/vendor/icanboogie/inflector/tests/cases/camel_to_underscore.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array
-(
-	"Product"               => "product",
-	"SpecialGuest"          => "special_guest",
-	"ApplicationController" => "application_controller",
-	"Area51Controller"      => "area51_controller"
-);
--- plugins/market/vendor/icanboogie/inflector/tests/cases/underscores_to_dashes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return array
-(
-	"street"                => "street",
-	"street_address"        => "street-address",
-	"person_street_address" => "person-street-address"
-);
