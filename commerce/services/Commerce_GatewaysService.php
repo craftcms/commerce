4546d920b1748fe4e5791c1d426bc12a695d7b9b
Luke Holder <lukemh@gmail.com>
2015-10-12 19:39:26 +0800

    Dummy conditional for available gateways.

--- commerce/services/Commerce_GatewaysService.php
+++ commerce/services/Commerce_GatewaysService.php
@@ -29,7 +29,51 @@ class Commerce_GatewaysService extends BaseApplicationComponent
      */
     private function _loadGateways()
     {
-        $adapters = craft()->plugins->call('registerCommerceGatewayAdapters');
+
+        $allAdapters = [
+            '\Commerce\Gateways\Omnipay\AuthorizeNet_AIM_GatewayAdapter',
+            '\Commerce\Gateways\Omnipay\AuthorizeNet_SIM_GatewayAdapter',
+            '\Commerce\Gateways\Omnipay\Buckaroo_Ideal_GatewayAdapter',
+            '\Commerce\Gateways\Omnipay\Buckaroo_PayPal_GatewayAdapter',
+            '\Commerce\Gateways\Omnipay\CardSave_GatewayAdapter',
+            '\Commerce\Gateways\Omnipay\Coinbase_GatewayAdapter',
+            '\Commerce\Gateways\Omnipay\Dummy_GatewayAdapter',
+            '\Commerce\Gateways\Omnipay\Eway_Rapid_GatewayAdapter',
+            '\Commerce\Gateways\Omnipay\FirstData_Connect_GatewayAdapter',
+            '\Commerce\Gateways\Omnipay\GoCardless_GatewayAdapter',
+            '\Commerce\Gateways\Omnipay\Manual_GatewayAdapter',
+            '\Commerce\Gateways\Omnipay\Migs_ThreeParty_GatewayAdapter',
+            '\Commerce\Gateways\Omnipay\Migs_TwoParty_GatewayAdapter',
+            '\Commerce\Gateways\Omnipay\Mollie_GatewayAdapter',
+            '\Commerce\Gateways\Omnipay\MultiSafepay_GatewayAdapter',
+            '\Commerce\Gateways\Omnipay\Netaxept_GatewayAdapter',
+            '\Commerce\Gateways\Omnipay\NetBanx_GatewayAdapter',
+            '\Commerce\Gateways\Omnipay\PayFast_GatewayAdapter',
+            '\Commerce\Gateways\Omnipay\Payflow_Pro_GatewayAdapter',
+            '\Commerce\Gateways\Omnipay\PaymentExpress_PxPay_GatewayAdapter',
+            '\Commerce\Gateways\Omnipay\PaymentExpress_PxPost_GatewayAdapter',
+            '\Commerce\Gateways\Omnipay\PayPal_Express_GatewayAdapter',
+            '\Commerce\Gateways\Omnipay\PayPal_Pro_GatewayAdapter',
+            '\Commerce\Gateways\Omnipay\Pin_GatewayAdapter',
+            '\Commerce\Gateways\Omnipay\SagePay_Direct_GatewayAdapter',
+            '\Commerce\Gateways\Omnipay\SagePay_Server_GatewayAdapter',
+            '\Commerce\Gateways\Omnipay\SecurePay_DirectPost_GatewayAdapter',
+            '\Commerce\Gateways\Omnipay\Stripe_GatewayAdapter',
+            '\Commerce\Gateways\Omnipay\TargetPay_Directebanking_GatewayAdapter',
+            '\Commerce\Gateways\Omnipay\TargetPay_Ideal_GatewayAdapter',
+            '\Commerce\Gateways\Omnipay\TargetPay_Mrcash_GatewayAdapter',
+            '\Commerce\Gateways\Omnipay\TwoCheckout_GatewayAdapter',
+            '\Commerce\Gateways\Omnipay\WorldPay_GatewayAdapter',
+        ];
+
+        $adapters = [];
+        // Dummy conditional for activation
+        if(true){
+            $adapters = craft()->plugins->call('registerCommerceGatewayAdapters');
+            $adapters['Commerce'] = $allAdapters;
+        }else{
+            $adapters['commerce'] = ['\Commerce\Gateways\Omnipay\Dummy_GatewayAdapter'];
+        }
 
         foreach ($adapters as $adaptersByPlugin) {
             foreach ($adaptersByPlugin as $class) {
