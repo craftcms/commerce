7d57017cb83603f1ab6113b9e02f5b0fc1a0815f
Luke Holder <lukemh@gmail.com>
2015-10-26 15:55:34 +0800

    Order transaction row info.

--- src/resources/js/classes/TableRowAdditionalInfoIcon.js
+++ src/resources/js/classes/TableRowAdditionalInfoIcon.js
@@ -22,22 +22,14 @@ Craft.Commerce.TableRowAdditionalInfoIcon = Garnish.Base.extend(
 
                 var item = this.$icon.closest('.infoRow');
                 var $hudbody = $("<div/>");
-                var $html = $("<div><h2>Details</h2><table class='data fullwidth'><tbody>" +
-                    "<tr><td><strong>" + Craft.t('Description') + "</strong></td><td> " + item.data('description') + "</td></tr>" +
-                    "<tr><td><strong>" + Craft.t('Quantity') + "</strong></td><td> " + item.data('qty') + "</td></tr>" +
-                    "<tr><td><strong>" + Craft.t('Tax Category') + "</strong></td><td><a href='" + item.data('taxcategoryurl') + "'>" + item.data('taxcategory') + "</a></td></tr>" +
-                    "<tr><td><strong>" + Craft.t('Price') + "</strong></td><td> " + item.data('price') + "</td></tr>" +
-                    "<tr><td><strong>" + Craft.t('Sale Amount') + "</strong></td><td> " + item.data('saleamount') + "</td></tr>" +
-                    "<tr><td><strong>" + Craft.t('Sale Price') + "</strong></td><td> " + item.data('saleprice') + "</td></tr>" +
-                    "<tr><td><strong>" + Craft.t('Tax') + "</strong></td><td> " + item.data('tax') + "</td></tr>" +
-                    "<tr><td><strong>" + Craft.t('Shipping Cost') + "</strong></td><td> " + item.data('shippingcost') + "</td></tr>" +
-                    "<tr><td><strong>" + Craft.t('Discount') + "</strong></td><td> " + item.data('discount') + "</td></tr>" +
-                    "<tr><td><strong>" + Craft.t('Total') + "</strong></td><td> " + item.data('total') + "</td></tr>" +
-                    "<tr><td><strong>" + Craft.t('Note') + "</strong></td><td> " + item.data('note') + "</td></tr>" +
-                    "<tr><td><strong>" + Craft.t('On Sale?') + "</strong></td><td> " + item.data('onsale') + "</td></tr>" +
-                    "</tbody></table></div>");
+                var $table = $("<div><h2>Details</h2><table class='data fullwidth'><tbody></tbody></table>").appendTo($hudbody);
 
-                $hudbody.append($html);
+                var $tbody = $table.find('tbody');
+
+                var rows = item.data('inforow');
+                for(i=0;i<rows.length;i++){
+                    $('<tr><td><strong>'+Craft.t(rows[i].label)+'</strong></td><td>'+item.data(rows[i].attribute)+'</td></tr>').appendTo($tbody);
+                }
 
                 this.hud = new Garnish.HUD(this.$icon, $hudbody, {
                     hudClass: 'hud',
