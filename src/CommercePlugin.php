13aae0d9454da96bee8741f6bf959fd2d81f0d94
Luke Holder <lukemh@gmail.com>
2015-10-28 16:05:08 +0800

    Remove Market after upgrading from market to commerce

--- src/CommercePlugin.php
+++ src/CommercePlugin.php
@@ -98,6 +98,8 @@ class CommercePlugin extends BasePlugin
                         $migration->up();
                     }
 
+                    craft()->db->createCommand()->delete('plugins', 'class=Market');
+
                     CommerceDbHelper::commitStackedTransaction();
                 } catch (Exception $e) {
                     CommerceDbHelper::rollbackStackedTransaction();
