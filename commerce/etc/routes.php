012cc66888b0f6c36c1f80b639c3acda7f370d6b
Luke Holder <lukemh@gmail.com>
2015-11-06 10:52:51 +0800

    Fix typo in route

--- commerce/etc/routes.php
+++ commerce/etc/routes.php
@@ -66,5 +66,5 @@ return [
     // Customers
     'commerce/customers' => ['action' => 'commerce/customers/index'],
     'commerce/customers/(?P<id>\d+)' => ['action' => 'commerce/customers/edit'],
-    'commerce/addresses/(?P<id>\d+)' => ['action' => 'commerce/addresss/edit'],
+    'commerce/addresses/(?P<id>\d+)' => ['action' => 'commerce/addresses/edit'],
 ];
