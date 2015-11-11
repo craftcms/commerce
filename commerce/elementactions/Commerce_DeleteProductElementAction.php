745bbcdada9307de116d1f187cdbe398aea471ee
Luke Holder <lukemh@gmail.com>
2015-11-12 01:00:59 +0800

    Fixed bug where deleting products with the delete element action did not delete associated variants.

---
+++ commerce/elementactions/Commerce_DeleteProductElementAction.php
@@ -0,0 +1,82 @@
+<?php
+namespace Craft;
+
+/**
+ * Delete Element Action
+ *
+ * @author    Pixel & Tonic, Inc. <support@pixelandtonic.com>
+ * @copyright Copyright (c) 2014, Pixel & Tonic, Inc.
+ * @license   http://buildwithcraft.com/license Craft License Agreement
+ * @link      http://buildwithcraft.com
+ * @package   craft.app.elementactions
+ * @since     2.3
+ */
+class Commerce_DeleteProductElementAction extends BaseElementAction
+{
+	// Public Methods
+	// =========================================================================
+
+	/**
+	 * @inheritDoc IComponentType::getName()
+	 *
+	 * @return string
+	 */
+	public function getName()
+	{
+		return Craft::t('Delete…');
+	}
+
+	/**
+	 * @inheritDoc IElementAction::isDestructive()
+	 *
+	 * @return bool
+	 */
+	public function isDestructive()
+	{
+		return true;
+	}
+
+	/**
+	 * @inheritDoc IElementAction::getConfirmationMessage()
+	 *
+	 * @return string|null
+	 */
+	public function getConfirmationMessage()
+	{
+		return $this->getParams()->confirmationMessage;
+	}
+
+	/**
+	 * @inheritDoc IElementAction::performAction()
+	 *
+	 * @param ElementCriteriaModel $criteria
+	 *
+	 * @return bool
+	 */
+	public function performAction(ElementCriteriaModel $criteria)
+	{
+		foreach($criteria->find() as $product){
+			craft()->commerce_products->deleteProduct($product);
+		}
+
+		$this->setMessage($this->getParams()->successMessage);
+
+		return true;
+	}
+
+	// Protected Methods
+	// =========================================================================
+
+	/**
+	 * @inheritDoc BaseElementAction::defineParams()
+	 *
+	 * @return array
+	 */
+	protected function defineParams()
+	{
+		return array(
+			'confirmationMessage' => array(AttributeType::String),
+			'successMessage'      => array(AttributeType::String),
+		);
+	}
+}
