a6abfa55053a7ef06f30f1239ad203a2ff06ae4e
Luke Holder <lukemh@gmail.com>
2016-03-02 14:24:33 +0800

    Shipping zones formatting. Added deprecation warning to `ShippingRuleModel::getState()` and  `ShippingRuleModel::getCountry()`

--- src/controllers/Commerce_ShippingZonesController.php
+++ src/controllers/Commerce_ShippingZonesController.php
@@ -13,109 +13,126 @@ namespace Craft;
  */
 class Commerce_ShippingZonesController extends Commerce_BaseAdminController
 {
-    /**
-     * @throws HttpException
-     */
-    public function actionIndex()
-    {
-        $shippingZones = craft()->commerce_shippingZones->getAllShippingZones();
-        $this->renderTemplate('commerce/settings/shippingzones/index',
-            compact('shippingZones'));
-    }
+	/**
+	 * @throws HttpException
+	 */
+	public function actionIndex()
+	{
+		$shippingZones = craft()->commerce_shippingZones->getAllShippingZones();
+		$this->renderTemplate('commerce/settings/shippingzones/index',
+			compact('shippingZones'));
+	}
 
-    /**
-     * Create/Edit ShippingZone
-     *
-     * @param array $variables
-     *
-     * @throws HttpException
-     */
-    public function actionEdit(array $variables = [])
-    {
-        if (empty($variables['shippingZone'])) {
-            if (!empty($variables['id'])) {
-                $id = $variables['id'];
-                $variables['shippingZone'] = craft()->commerce_shippingZones->getShippingZoneById($id);
+	/**
+	 * Create/Edit ShippingZone
+	 *
+	 * @param array $variables
+	 *
+	 * @throws HttpException
+	 */
+	public function actionEdit(array $variables = [])
+	{
+		if (empty($variables['shippingZone']))
+		{
+			if (!empty($variables['id']))
+			{
+				$id = $variables['id'];
+				$variables['shippingZone'] = craft()->commerce_shippingZones->getShippingZoneById($id);
 
-                if (!$variables['shippingZone']) {
-                    throw new HttpException(404);
-                }
-            } else {
-                $variables['shippingZone'] = new Commerce_ShippingZoneModel();
-            };
-        }
+				if (!$variables['shippingZone'])
+				{
+					throw new HttpException(404);
+				}
+			}
+			else
+			{
+				$variables['shippingZone'] = new Commerce_ShippingZoneModel();
+			};
+		}
 
-        if (!empty($variables['id'])) {
-            $variables['title'] = $variables['shippingZone']->name;
-        } else {
-            $variables['title'] = Craft::t('Create a shipping zone');
-        }
+		if (!empty($variables['id']))
+		{
+			$variables['title'] = $variables['shippingZone']->name;
+		}
+		else
+		{
+			$variables['title'] = Craft::t('Create a shipping zone');
+		}
 
-        $countries = craft()->commerce_countries->getAllCountries();
-        $states = craft()->commerce_states->getAllStates();
+		$countries = craft()->commerce_countries->getAllCountries();
+		$states = craft()->commerce_states->getAllStates();
 
-        $variables['countries'] = \CHtml::listData($countries, 'id', 'name');
-        $variables['states'] = \CHtml::listData($states, 'id', 'name');
+		$variables['countries'] = \CHtml::listData($countries, 'id', 'name');
+		$variables['states'] = \CHtml::listData($states, 'id', 'name');
 
-        $this->renderTemplate('commerce/settings/shippingzones/_edit', $variables);
-    }
+		$this->renderTemplate('commerce/settings/shippingzones/_edit', $variables);
+	}
 
-    /**
-     * @throws HttpException
-     */
-    public function actionSave()
-    {
-        $this->requirePostRequest();
+	/**
+	 * @throws HttpException
+	 */
+	public function actionSave()
+	{
+		$this->requirePostRequest();
 
-        $shippingZone = new Commerce_ShippingZoneModel();
+		$shippingZone = new Commerce_ShippingZoneModel();
 
-        // Shared attributes
-        $shippingZone->id = craft()->request->getPost('shippingZoneId');
-        $shippingZone->name = craft()->request->getPost('name');
-        $shippingZone->description = craft()->request->getPost('description');
-        $shippingZone->countryBased = craft()->request->getPost('countryBased');
-        $shippingZone->countryBased = craft()->request->getPost('countryBased');
-        $countryIds = craft()->request->getPost('countries', []);
-        $stateIds = craft()->request->getPost('states', []);
+		// Shared attributes
+		$shippingZone->id = craft()->request->getPost('shippingZoneId');
+		$shippingZone->name = craft()->request->getPost('name');
+		$shippingZone->description = craft()->request->getPost('description');
+		$shippingZone->countryBased = craft()->request->getPost('countryBased');
+		$shippingZone->countryBased = craft()->request->getPost('countryBased');
+		$countryIds = craft()->request->getPost('countries', []);
+		$stateIds = craft()->request->getPost('states', []);
 
-        // Save it
-        if (craft()->commerce_shippingZones->saveShippingZone($shippingZone, $countryIds, $stateIds)) {
-            if (craft()->request->isAjaxRequest()) {
-                $this->returnJson([
-                    'success' => true,
-                    'id' => $shippingZone->id,
-                    'name' => $shippingZone->name,
-                ]);
-            } else {
-                craft()->userSession->setNotice(Craft::t('Shipping zone saved.'));
-                $this->redirectToPostedUrl($shippingZone);
-            }
-        } else {
-            if (craft()->request->isAjaxRequest()) {
-                $this->returnJson([
-                    'errors' => $shippingZone->getErrors()
-                ]);
-            } else {
-                craft()->userSession->setError(Craft::t('Couldn’t save shipping zone.'));
-            }
-        }
+		// Save it
+		if (craft()->commerce_shippingZones->saveShippingZone($shippingZone, $countryIds, $stateIds))
+		{
+			if (craft()->request->isAjaxRequest())
+			{
+				$this->returnJson([
+					'success' => true,
+					'id'      => $shippingZone->id,
+					'name'    => $shippingZone->name,
+				]);
+			}
+			else
+			{
+				craft()->userSession->setNotice(Craft::t('Shipping zone saved.'));
+				$this->redirectToPostedUrl($shippingZone);
+			}
+		}
+		else
+		{
+			if (craft()->request->isAjaxRequest())
+			{
+				$this->returnJson([
+					'errors' => $shippingZone->getErrors()
+				]);
+			}
+			else
+			{
+				craft()->userSession->setError(Craft::t('Couldn’t save shipping zone.'));
+			}
+		}
 
-        // Send the model back to the template
-        craft()->urlManager->setRouteVariables(['shippingZone' => $shippingZone]);
-    }
+		// Send the model back to the template
+		craft()->urlManager->setRouteVariables(['shippingZone' => $shippingZone]);
+	}
 
-    /**
-     * @throws HttpException
-     */
-    public function actionDelete()
-    {
-        $this->requirePostRequest();
-        $this->requireAjaxRequest();
+	/**
+	 * @throws HttpException
+	 */
+	public function actionDelete()
+	{
+		$this->requirePostRequest();
+		$this->requireAjaxRequest();
 
-        $id = craft()->request->getRequiredPost('id');
+		$id = craft()->request->getRequiredPost('id');
 
-        craft()->commerce_shippingZones->deleteShippingZoneById($id);
-        $this->returnJson(['success' => true]);
-    }
+		craft()->commerce_shippingZones->deleteShippingZoneById($id);
+		$this->returnJson(['success' => true]);
+	}
 
 }
