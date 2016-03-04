02284af61135b136bd561f20592587eaaa95242d
Brandon Kelly <brandon@pixelandtonic.com>
2016-03-04 15:15:57 -0800

    Move all the new revenue chart data stuff into the controller

--- src/controllers/Commerce_ChartsController.php
+++ src/controllers/Commerce_ChartsController.php
@@ -30,10 +30,81 @@ class Commerce_ChartsController extends ElementIndexController
         $endDate = DateTime::createFromString($endDateParam, craft()->timezone);
         $endDate->modify('+1 day');
 
+        $intervalUnit = ChartHelper::getRunChartIntervalUnit($startDate, $endDate);
+
+        // Prep the query
         $criteria = $this->getElementCriteria();
+        $criteria->limit = null;
 
-        $revenueReport = craft()->commerce_charts->getRevenueReport($criteria, $startDate, $endDate);
+        $query = craft()->elements->buildElementsQuery($criteria)
+            ->select('sum(orders.totalPrice) as value');
 
-        $this->returnJson($revenueReport);
+        // Get the chart data table
+        $dataTable = ChartHelper::getRunChartDataFromQuery($query, $startDate, $endDate, 'orders.dateOrdered', [
+            'intervalUnit' => $intervalUnit,
+            'valueLabel' => Craft::t('Revenue'),
+            'valueType' => 'currency',
+        ]);
+
+        // Get the total revenue
+        $total = 0;
+
+        foreach($dataTable['rows'] as $row)
+        {
+            $total = $total + $row[1];
+        }
+
+        // Return everything
+        $currency = craft()->commerce_settings->getOption('defaultCurrency');
+        $totalHtml = craft()->numberFormatter->formatCurrency($total, strtoupper($currency));
+
+        $this->returnJson(array(
+            'dataTable' => $dataTable,
+            'total' => $total,
+            'totalHtml' => $totalHtml,
+
+            'formats' => ChartHelper::getFormats(),
+            'orientation' => craft()->locale->getOrientation(),
+            'scale' => $intervalUnit,
+            'localeDefinition' => [
+                'currency' => $this->_getLocaleDefinitionCurrency(),
+            ],
+        ));
+    }
+
+    /**
+     * Returns D3 currency format locale definition.
+     *
+     * @return string
+     */
+    private function _getLocaleDefinitionCurrency()
+    {
+        $currency = craft()->commerce_settings->getOption('defaultCurrency');
+
+        $currencySymbol = craft()->locale->getCurrencySymbol($currency);
+        $currencyFormat = craft()->locale->getCurrencyFormat();
+
+        if(strpos($currencyFormat, ";") > 0)
+        {
+            $currencyFormatArray = explode(";", $currencyFormat);
+            $currencyFormat = $currencyFormatArray[0];
+        }
+
+        $pattern = '/[#0,.]/';
+        $replacement = '';
+        $currencyFormat = preg_replace($pattern, $replacement, $currencyFormat);
+
+        if(strpos($currencyFormat, "¤") === 0)
+        {
+            // symbol at beginning
+            $currencyD3Format = [str_replace('¤', $currencySymbol, $currencyFormat), ''];
+        }
+        else
+        {
+            // symbol at the end
+            $currencyD3Format = ['', str_replace('¤', $currencySymbol, $currencyFormat)];
+        }
+
+        return $currencyD3Format;
     }
 }
