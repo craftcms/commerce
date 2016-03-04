7fbcd1218c1ed4ce8639d2800077fc5a36cdf76e
Brandon Kelly <brandon@pixelandtonic.com>
2016-03-04 13:51:37 -0800

    Use ChartHelper::getRunChartDataFromQuery()

--- commerce/services/Commerce_ChartsService.php
+++ commerce/services/Commerce_ChartsService.php
@@ -27,7 +27,8 @@ class Commerce_ChartsService extends BaseApplicationComponent
      */
     public function getRevenueReport($criteria, $startDate, $endDate)
     {
-        $dataTable = $this->getRevenueDataTable($startDate, $endDate, $criteria);
+	    $intervalUnit = ChartHelper::getRunChartIntervalUnit($startDate, $endDate);
+        $dataTable = $this->getRevenueDataTable($startDate, $endDate, $intervalUnit, $criteria);
 
         $total = 0;
 
@@ -46,7 +47,7 @@ class Commerce_ChartsService extends BaseApplicationComponent
 
 	        'formats' => craft()->charts->getFormats(),
             'orientation' => craft()->locale->getOrientation(),
-            'scale' => ChartHelper::getDateChartScale($startDate, $endDate),
+            'scale' => $intervalUnit,
             'localeDefinition' => [
                 'currency' => $this->getLocaleDefinitionCurrency(),
             ],
@@ -61,153 +62,23 @@ class Commerce_ChartsService extends BaseApplicationComponent
      *
      * @param string $startDate
      * @param string $endDate
+     * @param string $intervalUnit
      * @param int|null $userGroupId
      *
      * @return array Returns a data table (array of columns and rows)
      */
-    private function getRevenueDataTable($startDate, $endDate, $criteria)
+    private function getRevenueDataTable($startDate, $endDate, $intervalUnit, $criteria)
     {
-
-        $scale = ChartHelper::getDateChartScale($startDate, $endDate);
-        $scaleFormat = craft()->charts->getScaleDateFormat($scale);
-
+        // Convert the criteria into an element query
         $criteria->limit = null;
+        $query = craft()->elements->buildElementsQuery($criteria)
+            ->select('sum(orders.totalPrice) as value');
 
-        $query = craft()->elements->buildElementsQuery($criteria);
-
-        $query->select('DATE_FORMAT(orders.dateOrdered, "'.$scaleFormat.'") as date, sum(orders.totalPrice) as revenue');
-        $query->andWhere(array('and', 'orders.dateOrdered > :startDate', 'orders.dateOrdered < :endDate'), array(':startDate' => $startDate->mySqlDateTime(), ':endDate' => $endDate->mySqlDateTime()));
-
-        switch ($scale)
-        {
-            case 'year':
-                $query->group('YEAR(orders.dateOrdered)');
-                break;
-
-            case 'month':
-                $query->group('YEAR(orders.dateOrdered), MONTH(orders.dateOrdered)');
-                break;
-
-            case 'hour':
-                $query->group('YEAR(orders.dateOrdered), MONTH(orders.dateOrdered), DAY(orders.dateOrdered), HOUR(orders.dateOrdered)');
-                break;
-
-            case 'day':
-                $query->group('YEAR(orders.dateOrdered), MONTH(orders.dateOrdered), DAY(orders.dateOrdered)');
-                break;
-        }
-
-        // $query->join('select date, revenue from DATE_ADD(date, INTERVAL expr type)');
-
-        $results = $query->queryAll();
-
-        return $this->parseResultsToDataTable($startDate, $endDate, $results);
-    }
-
-    /**
-     * Returns report as a data table
-     *
-     * @param string $startDate
-     * @param string $endDate
-     * @param array $results
-     *
-     * @return array
-     */
-    private function parseResultsToDataTable($startDate, $endDate, $results)
-    {
-        $scale = ChartHelper::getDateChartScale($startDate, $endDate);
-	    $scaleFormat = craft()->charts->getScaleDateFormat($scale);
-
-
-        // columns
-
-        $columns = [];
-
-        switch ($scale)
-        {
-            case 'hour':
-                $xType = 'datetime';
-                break;
-
-            default:
-                $xType = 'date';
-                break;
-        }
-
-        $columns[] = [
-            'type' => $xType,
-            'label' => Craft::t('Date'),
-        ];
-
-        $columns[] = [
-            'type' => 'currency',
-            'label' => Craft::t('Revenue'),
-        ];
-
-
-        // rows
-
-        $rows = [];
-
-        $timezone = new \DateTimeZone(craft()->timezone);
-
-
-        switch($scale)
-        {
-            case 'year':
-            $cursorCurrent = new DateTime($startDate, $timezone);
-            $cursorCurrent = new DateTime($cursorCurrent->format('Y-01-01'), $timezone);
-            break;
-
-            case 'month':
-            $cursorCurrent = new DateTime($startDate, $timezone);
-            $cursorCurrent = new DateTime($cursorCurrent->format('Y-m-01'), $timezone);
-            break;
-
-            default:
-            $cursorCurrent = new DateTime($startDate, $timezone);
-        }
-
-        while($cursorCurrent->getTimestamp() < $endDate->getTimestamp())
-        {
-            switch($scale)
-            {
-                case 'hour':
-                $cursorFormat = 'Y-m-d H:i';
-                break;
-
-                default:
-                $cursorFormat = 'Y-m-d';
-            }
-
-            $cursorStart = new DateTime($cursorCurrent->format($cursorFormat), $timezone);
-            $cursorCurrent->modify('+1 '.$scale);
-
-            $cursorEnd = $cursorCurrent;
-
-            $row = [
-                strftime($scaleFormat, $cursorStart->getTimestamp()), // date
-                0 // revenue
-            ];
-
-            foreach($results as $result)
-            {
-                if($result['date'] == strftime($scaleFormat, $cursorStart->getTimestamp()))
-                {
-                    $row = [
-                        $result['date'], // date
-                        (float) $result['revenue'] // revenue
-                    ];
-                }
-            }
-
-            $rows[] = $row;
-        }
-
-        return array(
-            'columns' => $columns,
-            'rows' => $rows,
-        );
+        return ChartHelper::getRunChartDataFromQuery($query, $startDate, $endDate, 'orders.dateOrdered', [
+	        'intervalUnit' => $intervalUnit,
+	        'valueLabel' => Craft::t('Revenue'),
+	        'valueType' => 'currency',
+        ]);
     }
 
 	/**
