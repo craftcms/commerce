2f95c0cdfcb07ce39d19adfa060cf951f425fc1e
Luke Holder <lukemh@gmail.com>
2016-03-07 10:26:01 +0800

    Ad deprecation warnings to CartPaymentController

--- commerce/controllers/Commerce_CartPaymentController.php
+++ commerce/controllers/Commerce_CartPaymentController.php
@@ -18,6 +18,19 @@ class Commerce_CartPaymentController extends Commerce_BaseFrontEndController
      */
     public function actionPay()
     {
+
+        craft()->deprecator->log('Commerce_CartPaymentController::actionPay():removed', 'You should no longer use controller form action `cartPayment/pay` to pay and complete carts. Controller action `payments/pay` should be used.');
+
         $this->forward('commerce/payments/pay');
+    }
+
+    /**
+     * @throws HttpException
+     */
+    public function actionCompletePayment()
+    {
+        craft()->deprecator->log('Commerce_CartPaymentController::actionCompletePayment():removed', 'You should no longer use controller form action `cartPayment/completePayment` to complete payments. Controller action `payments/completePayment` should be used.');
+
+        $this->forward('commerce/payments/completePayment');
     }
 }
