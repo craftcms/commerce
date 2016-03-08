67d42fbbf7b7343cea2bff20f243a81c931c3d75
Brandon Kelly <brandon@pixelandtonic.com>
2016-03-08 06:22:50 -0800

    Only allow the Orders/Revenue widgets to be selected by users that have permission to manage orders

--- commerce/widgets/Commerce_RevenueWidget.php
+++ commerce/widgets/Commerce_RevenueWidget.php
@@ -8,6 +8,17 @@ class Commerce_RevenueWidget extends BaseWidget
     // =========================================================================
 
     /**
+     * @inheritDoc IComponentType::isSelectable()
+     *
+     * @return bool
+     */
+    public function isSelectable()
+    {
+        // This widget is only available to users that can manage orders
+        return craft()->userSession->checkPermission('commerce-manageOrders');
+    }
+
+    /**
      * @inheritDoc IComponentType::getName()
      *
      * @return string
