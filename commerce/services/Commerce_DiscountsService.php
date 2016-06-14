e04542bdee3f7f3fb108dbfe9c4ed9ea259ff589
Luke Holder <lukemh@gmail.com>
2016-06-14 08:51:26 +0800

    Move check of 'per email limit' to discount adjuster

--- commerce/services/Commerce_DiscountsService.php
+++ commerce/services/Commerce_DiscountsService.php
@@ -194,29 +194,25 @@ class Commerce_DiscountsService extends BaseApplicationComponent
 		    $cart = craft()->commerce_cart->getCart();
 		    $email = $cart->email;
 
-		    if (!$email && $model->perEmailLimit)
+		    if ($email)
 		    {
-			    $error = Craft::t('This coupon can only be used once we know your email address');
+			    $previousOrders = craft()->commerce_orders->getOrdersByEmail($email);
 
-			    return false;
-		    }
-
-		    $previousOrders = craft()->commerce_orders->getOrdersByEmail($email);
-
-		    $usedCount = 0;
-		    foreach ($previousOrders as $order)
-		    {
-			    if ($order->couponCode == $code)
+			    $usedCount = 0;
+			    foreach ($previousOrders as $order)
 			    {
-				    $usedCount = $usedCount + 1;
+				    if ($order->couponCode == $code)
+				    {
+					    $usedCount = $usedCount + 1;
+				    }
 			    }
-		    }
 
-		    if ($usedCount >= $model->perEmailLimit)
-		    {
-			    $error = Craft::t('This coupon limited to '.$model->perEmailLimit.' uses.');
+			    if ($usedCount >= $model->perEmailLimit)
+			    {
+				    $error = Craft::t('This coupon limited to '.$model->perEmailLimit.' uses.');
 
-			    return false;
+				    return false;
+			    }
 		    }
 	    }
 
