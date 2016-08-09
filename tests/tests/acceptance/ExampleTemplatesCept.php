66aba6641a7ada373cbe628f39a5374d1ba3feae
Benjamin David <ben@dukt.net>
2016-08-09 11:35:25 +0200

    Updated ExampleTemplateCept to expect URLs beginning with `/shop` instead of `/commerce`

--- tests/tests/acceptance/ExampleTemplatesCept.php
+++ tests/tests/acceptance/ExampleTemplatesCept.php
@@ -3,25 +3,25 @@
 $I = new AcceptanceTester($scenario);
 
 $I->wantTo('Ensure that example templates are working.');
-$I->amOnPage('/commerce'); 
+$I->amOnPage('/shop');
 $I->see('These are the Craft Commerce example templates');
 
-$I->amOnPage('/commerce/products');
+$I->amOnPage('/shop/products');
 $I->see('Parka with Stripes on Back');
 $I->see('Romper for a Red Eye');
 $I->see('A New Toga');
 $I->click('Add to cart');
 
-$I->seeInCurrentUrl('/commerce/cart');
+$I->seeInCurrentUrl('/shop/cart');
 $I->see('Cart updated.');
 
 $I->click('Checkout');
 
-$I->seeInCurrentUrl('/commerce/checkout');
+$I->seeInCurrentUrl('/shop/checkout');
 $I->fillField(['name' => 'email'], 'test@test.com'); 
 $I->click('Continue as Guest');
 
-$I->seeInCurrentUrl('/commerce/checkout/addresses');
+$I->seeInCurrentUrl('/shop/checkout/addresses');
 $I->fillField(['name' => 'shippingAddress[firstName]'], 'Luke'); 
 $I->fillField(['name' => 'shippingAddress[lastName]'], 'Holder');
 $I->fillField(['name' => 'shippingAddress[businessName]'], 'P&T');
@@ -49,7 +49,7 @@ $I->fillField(['name' => 'lastName'], 'Holder');
 $I->fillField(['name' => 'number'], '4242424242424242');
 $I->fillField(['name' => 'cvv'], '123');
 $I->click('Pay');
-$I->amOnPage('/commerce/customer/order')
+$I->amOnPage('/shop/customer/order')
 
 
 ?>
