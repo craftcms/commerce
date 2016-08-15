1b083d2121704665a446e3d3425ab7fc99ff7a60
Luke Holder <lukemh@gmail.com>
2016-08-15 13:08:53 +0800

    Fixed bug where addresses with special chars were causing the edit address modal to show a blank address.

--- commerce/Commerce/Extensions/CommerceTwigExtension.php
+++ commerce/Commerce/Extensions/CommerceTwigExtension.php
@@ -28,7 +28,8 @@ class CommerceTwigExtension extends \Twig_Extension
     public function getFilters()
     {
         $returnArray['commercePercent'] = new \Twig_Filter_Method($this, 'percent');
-
+        $returnArray['json_encode_filtered'] = new \Twig_Filter_Method($this, 'jsonEncodeFiltered');
+        
         return $returnArray;
     }
 
@@ -43,6 +44,54 @@ class CommerceTwigExtension extends \Twig_Extension
         $percentSign = $localeData->getNumberSymbol('percentSign');
 
         return $this->decimal($string) . "" . $percentSign;
+    }
+
+    public function jsonEncodeFiltered($input)
+    {
+        $array = $this->recursiveSanitizeArray($input);
+
+        return json_encode($array);
+    }
+
+    public static function sanitize($input)
+    {
+        $sanitized = $input;
+
+        if ( ! is_int($sanitized)) {
+            $sanitized = filter_var($sanitized, FILTER_SANITIZE_SPECIAL_CHARS);
+        } else {
+            $newValue = filter_var($sanitized, FILTER_SANITIZE_SPECIAL_CHARS);
+
+            if (is_numeric($newValue))
+            {
+                $sanitized = intval($newValue);
+            } else {
+                $sanitized = $newValue;
+            }
+        }
+
+        return $sanitized;
+    }
+
+    public function recursiveSanitizeArray($array)
+    {
+        $finalArray = [];
+
+        foreach ($array as $key => $value)
+        {
+            $newKey = self::sanitize($key);
+
+            if (is_array($value))
+            {
+                $finalArray[$newKey] = $this->recursiveSanitizeArray($value);
+            }
+            else
+            {
+                $finalArray[$newKey] = self::sanitize($value);
+            }
+        }
+
+        return $finalArray;
     }
 
 }
