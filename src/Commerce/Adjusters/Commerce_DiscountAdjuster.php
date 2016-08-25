47d4bfe13403bc678f6b8067ed34d71685a566a4
Luke Holder <lukemh@gmail.com>
2016-08-25 10:31:03 +0800

    Formatting

--- src/Commerce/Adjusters/Commerce_DiscountAdjuster.php
+++ src/Commerce/Adjusters/Commerce_DiscountAdjuster.php
@@ -18,204 +18,228 @@ use Craft\StringHelper;
  */
 class Commerce_DiscountAdjuster implements Commerce_AdjusterInterface
 {
-    const ADJUSTMENT_TYPE = 'Discount';
+	const ADJUSTMENT_TYPE = 'Discount';
 
-    /**
-     * @param Commerce_OrderModel $order
-     * @param Commerce_LineItemModel[] $lineItems
-     *
-     * @return \Craft\Commerce_OrderAdjustmentModel[]
-     */
-    public function adjust(Commerce_OrderModel &$order, array $lineItems = [])
-    {
-        if (empty($lineItems)) {
-            return [];
-        }
+	/**
+	 * @param Commerce_OrderModel      $order
+	 * @param Commerce_LineItemModel[] $lineItems
+	 *
+	 * @return \Craft\Commerce_OrderAdjustmentModel[]
+	 */
+	public function adjust(Commerce_OrderModel &$order, array $lineItems = [])
+	{
+		if (empty($lineItems))
+		{
+			return [];
+		}
 
-        $discounts = \Craft\craft()->commerce_discounts->getAllDiscounts([
-            'condition' => '(code = :code OR code IS NULL) and enabled = :enabled',
-            'params' => ['code' => $order->couponCode, 'enabled' => true],
-        ]);
-        $adjustments = [];
-        foreach ($discounts as $discount) {
-            if ($adjustment = $this->getAdjustment($order, $lineItems, $discount)) {
-                $adjustments[] = $adjustment;
-            }
-        }
-        return $adjustments;
-    }
+		$discounts = \Craft\craft()->commerce_discounts->getAllDiscounts(['condition' => '(code = :code OR code IS NULL) and enabled = :enabled', 'params' => ['code' => $order->couponCode, 'enabled' => true],]);
+		$adjustments = [];
+		foreach ($discounts as $discount)
+		{
+			if ($adjustment = $this->getAdjustment($order, $lineItems, $discount))
+			{
+				$adjustments[] = $adjustment;
+			}
+		}
 
-    /**
-     * @param Commerce_OrderModel $order
-     * @param Commerce_LineItemModel[] $lineItems
-     * @param Commerce_DiscountModel $discount
-     *
-     * @return Commerce_OrderAdjustmentModel|false
-     */
-    private function getAdjustment(Commerce_OrderModel $order, array $lineItems, Commerce_DiscountModel $discount)
-    {
-        //preparing model
-        $adjustment = new Commerce_OrderAdjustmentModel;
-        $adjustment->type = self::ADJUSTMENT_TYPE;
-        $adjustment->name = $discount->name;
-        $adjustment->orderId = $order->id;
-        $adjustment->description = $discount->description ?: $this->getDescription($discount);
-        $adjustment->optionsJson = $discount->attributes;
-        $affectedLineIds = [];
+		return $adjustments;
+	}
 
-
-	    // Handle special coupon rules
-	    if ($order->couponCode == $discount->code)
-	    {
-		    // Since we will allow the coupon to be added to an anonymous cart with no email, we need to remove it
-		    // if a limit has been set.
-		    if ($order->email && $discount->perEmailLimit)
-		    {
-			    $previousOrders = \Craft\craft()->commerce_orders->getOrdersByEmail($order->email);
-
-			    $usedCount = 0;
-			    foreach ($previousOrders as $previousOrder)
-			    {
-				    if ($previousOrder->couponCode == $discount->code)
-				    {
-					    $usedCount = $usedCount + 1;
-				    }
-			    }
-
-			    if ($usedCount >= $discount->perEmailLimit)
-			    {
-				    $order->couponCode = "";
-
-				    return false;
-			    }
-		    }
-	    }
+	/**
+	 * @param Commerce_OrderModel      $order
+	 * @param Commerce_LineItemModel[] $lineItems
+	 * @param Commerce_DiscountModel   $discount
+	 *
+	 * @return Commerce_OrderAdjustmentModel|false
+	 */
+	private function getAdjustment(Commerce_OrderModel $order, array $lineItems, Commerce_DiscountModel $discount)
+	{
+		//preparing model
+		$adjustment = new Commerce_OrderAdjustmentModel;
+		$adjustment->type = self::ADJUSTMENT_TYPE;
+		$adjustment->name = $discount->name;
+		$adjustment->orderId = $order->id;
+		$adjustment->description = $discount->description ?: $this->getDescription($discount);
+		$adjustment->optionsJson = $discount->attributes;
+		$affectedLineIds = [];
 
 
-        //checking items
-        $matchingQty = 0;
-        $matchingTotal = 0;
-        $matchingLineIds = [];
-        foreach ($lineItems as $item) {
-            if (\Craft\craft()->commerce_discounts->matchLineItem($item, $discount)) {
-                $matchingLineIds[] = $item->id;
-                $matchingQty += $item->qty;
-                $matchingTotal += $item->getSubtotal();
-            }
-        }
+		// Handle special coupon rules
+		if ($order->couponCode == $discount->code)
+		{
+			// Since we will allow the coupon to be added to an anonymous cart with no email, we need to remove it
+			// if a limit has been set.
+			if ($order->email && $discount->perEmailLimit)
+			{
+				$previousOrders = \Craft\craft()->commerce_orders->getOrdersByEmail($order->email);
 
-        if (!$matchingQty) {
-            return false;
-        }
+				$usedCount = 0;
+				foreach ($previousOrders as $previousOrder)
+				{
+					if ($previousOrder->couponCode == $discount->code)
+					{
+						$usedCount = $usedCount + 1;
+					}
+				}
 
-        if ($matchingQty < $discount->purchaseQty) {
-            return false;
-        }
+				if ($usedCount >= $discount->perEmailLimit)
+				{
+					$order->couponCode = "";
 
-        if ($matchingTotal < $discount->purchaseTotal) {
-            return false;
-        }
+					return false;
+				}
+			}
+		}
 
-        // calculate discount (adjustment amount should be negative)
-        $amount = $discount->baseDiscount;
-        $amount += CommerceCurrencyHelper::round($discount->perItemDiscount * $matchingQty);
-        $amount += CommerceCurrencyHelper::round($discount->percentDiscount * $matchingTotal);
 
-        $shippingRemoved = 0;
+		//checking items
+		$matchingQty = 0;
+		$matchingTotal = 0;
+		$matchingLineIds = [];
+		foreach ($lineItems as $item)
+		{
+			if (\Craft\craft()->commerce_discounts->matchLineItem($item, $discount))
+			{
+				$matchingLineIds[] = $item->id;
+				$matchingQty += $item->qty;
+				$matchingTotal += $item->getSubtotal();
+			}
+		}
 
-        foreach ($lineItems as $item) {
-            if (in_array($item->id, $matchingLineIds)) {
+		if (!$matchingQty)
+		{
+			return false;
+		}
 
-	            $amountPerItem = CommerceCurrencyHelper::round($discount->perItemDiscount * $item->qty);
-	            $amountPercentage = CommerceCurrencyHelper::round($discount->percentDiscount * $item->getSubtotal());
-                $item->discount +=  $amountPerItem + $amountPercentage;
-                // If the discount is larger than the subtotal
-                // make the discount equal to the item, thus making the item free.
-                if (($item->discount * -1) > $item->getSubtotal()) {
-                    $item->discount = -$item->getSubtotal();
-                }
+		if ($matchingQty < $discount->purchaseQty)
+		{
+			return false;
+		}
 
-                if (!$item->purchasable->getIsPromotable()) {
-                    $item->discount = 0;
-                }
+		if ($matchingTotal < $discount->purchaseTotal)
+		{
+			return false;
+		}
 
-                $affectedLineIds[] = $item->id;
+		// calculate discount (adjustment amount should be negative)
+		$amount = $discount->baseDiscount;
+		$amount += CommerceCurrencyHelper::round($discount->perItemDiscount * $matchingQty);
+		$amount += CommerceCurrencyHelper::round($discount->percentDiscount * $matchingTotal);
 
-	            if ($discount->freeShipping) {
-		            $shippingRemoved = $shippingRemoved + $item->shippingCost;
-		            $item->shippingCost = 0;
-	            }
-            }
+		$shippingRemoved = 0;
 
-        }
+		foreach ($lineItems as $item)
+		{
+			if (in_array($item->id, $matchingLineIds))
+			{
+				$amountPerItem = CommerceCurrencyHelper::round($discount->perItemDiscount * $item->qty);
+				$amountPercentage = CommerceCurrencyHelper::round($discount->percentDiscount * $item->getSubtotal());
+				$item->discount += $amountPerItem + $amountPercentage;
+				// If the discount is larger than the subtotal
+				// make the discount equal to the item, thus making the item free.
+				if (($item->discount * -1) > $item->getSubtotal())
+				{
+					$item->discount = -$item->getSubtotal();
+				}
 
-        if ($discount->freeShipping) {
-            $shippingRemoved = $shippingRemoved + $order->baseShippingCost;
-            $order->baseShippingCost = 0;
-        }
+				if (!$item->purchasable->getIsPromotable())
+				{
+					$item->discount = 0;
+				}
 
-        $order->baseDiscount += $discount->baseDiscount;
+				$affectedLineIds[] = $item->id;
 
-        // only display adjustment if an amount was calculated
-        if ($amount || $shippingRemoved) {
-            // Record which line items this discount affected.
-            $adjustment->optionsJson = array_merge(['lineItemsAffected'=>$affectedLineIds],$adjustment->optionsJson);
-            $adjustment->amount = $amount + ($shippingRemoved * -1);
-            return $adjustment;
-        } else {
-            return false;
-        }
-    }
+				if ($discount->freeShipping)
+				{
+					$shippingRemoved = $shippingRemoved + $item->shippingCost;
+					$item->shippingCost = 0;
+				}
+			}
+		}
 
-    /**
-     * @param Commerce_DiscountModel $discount
-     *
-     * @return string "1$ and 5% per item and 10$ base rate"
-     */
-    private function getDescription(Commerce_DiscountModel $discount)
-    {
-        $description = '';
-        $currency = \Craft\craft()->commerce_currencies->getDefaultCurrencyIso();
+		if ($discount->freeShipping)
+		{
+			$shippingRemoved = $shippingRemoved + $order->baseShippingCost;
+			$order->baseShippingCost = 0;
+		}
 
-        if ($discount->perItemDiscount || $discount->percentDiscount) {
-            if ($discount->perItemDiscount) {
-                $description .= \Craft\craft()->numberFormatter->formatCurrency($discount->perItemDiscount * -1,$currency);
-            }
+		$order->baseDiscount += $discount->baseDiscount;
 
-            if ($discount->percentDiscount) {
-                if ($discount->perItemDiscount) {
-                    $description .= ' and ';
-                }
+		// only display adjustment if an amount was calculated
+		if ($amount || $shippingRemoved)
+		{
+			// Record which line items this discount affected.
+			$adjustment->optionsJson = array_merge(['lineItemsAffected' => $affectedLineIds], $adjustment->optionsJson);
+			$adjustment->amount = $amount + ($shippingRemoved * -1);
 
-                $description .= \Craft\craft()->numberFormatter->formatPercentage($discount->percentDiscount * -1 . '%');
-            }
+			return $adjustment;
+		}
+		else
+		{
+			return false;
+		}
+	}
 
-            $description .= ' per item ';
-        }
+	/**
+	 * @param Commerce_DiscountModel $discount
+	 *
+	 * @return string "1$ and 5% per item and 10$ base rate"
+	 */
+	private function getDescription(Commerce_DiscountModel $discount)
+	{
+		$description = '';
+		$currency = \Craft\craft()->commerce_currencies->getDefaultCurrencyIso();
 
-        if ($discount->baseDiscount) {
-            if ($description) {
-                $description .= 'and ';
-            }
-            $description .= \Craft\craft()->numberFormatter->formatCurrency($discount->baseDiscount * -1,$currency) . ' base rate ';
-        }
+		if ($discount->perItemDiscount || $discount->percentDiscount)
+		{
+			if ($discount->perItemDiscount)
+			{
+				$description .= \Craft\craft()->numberFormatter->formatCurrency($discount->perItemDiscount * -1, $currency);
+			}
 
-        if ($discount->freeShipping) {
-            if ($description) {
-                $description .= 'and ';
-            }
+			if ($discount->percentDiscount)
+			{
+				if ($discount->perItemDiscount)
+				{
+					$description .= ' and ';
+				}
 
-            $description .= 'free shipping ';
-        }
+				$description .= \Craft\craft()->numberFormatter->formatPercentage($discount->percentDiscount * -1 .'%');
+			}
 
-        if ($discount->code) {
-            if ($description) {
-                $description .= 'and ';
-            }
+			$description .= ' per item ';
+		}
 
-            $description .= 'using code '.StringHelper::toUpperCase($discount->code);
-        }
+		if ($discount->baseDiscount)
+		{
+			if ($description)
+			{
+				$description .= 'and ';
+			}
+			$description .= \Craft\craft()->numberFormatter->formatCurrency($discount->baseDiscount * -1, $currency).' base rate ';
+		}
 
-        return $description;
-    }
+		if ($discount->freeShipping)
+		{
+			if ($description)
+			{
+				$description .= 'and ';
+			}
+
+			$description .= 'free shipping ';
+		}
+
+		if ($discount->code)
+		{
+			if ($description)
+			{
+				$description .= 'and ';
+			}
+
+			$description .= 'using code '.StringHelper::toUpperCase($discount->code);
+		}
+
+		return $description;
+	}
 }
