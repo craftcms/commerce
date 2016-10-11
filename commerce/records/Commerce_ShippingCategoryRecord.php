edfb03cfde986f327e45f668676cc153e6e02de7
Luke Holder <lukemh@gmail.com>
2016-10-11 13:14:29 +0800

    Fix docblock @since tags for 1.2

--- commerce/records/Commerce_ShippingCategoryRecord.php
+++ commerce/records/Commerce_ShippingCategoryRecord.php
@@ -15,7 +15,7 @@ namespace Craft;
  * @license   https://craftcommerce.com/license Craft Commerce License Agreement
  * @see       https://craftcommerce.com
  * @package   craft.plugins.commerce.records
- * @since     1.0
+ * @since     1.2
  */
 class Commerce_ShippingCategoryRecord extends BaseRecord
 {
