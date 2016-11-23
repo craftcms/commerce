e3661a00c6d574ef2185a3f868158fc03d04bbad
Luke Holder <lukemh@gmail.com>
2016-11-23 11:33:51 +0800

    Added REST based Multi-safepay gateway. XML based Multi-safepay gateway deprecated.

---
+++ commerce/Commerce/Gateways/Omnipay/MultiSafepay_RestGatewayAdapter.php
@@ -0,0 +1,18 @@
+<?php
+namespace Commerce\Gateways\Omnipay;
+
+use Craft\BaseModel;
+use Omnipay\Common\Message\AbstractRequest as OmnipayRequest;
+
+class MultiSafepay_RestGatewayAdapter extends \Commerce\Gateways\CreditCardGatewayAdapter
+{
+    public function handle()
+    {
+        return 'MultiSafepay_Rest';
+    }
+
+    public function populateRequest(OmnipayRequest $request, BaseModel $paymentForm)
+    {
+        $request->setType('redirect');
+    }
+}
