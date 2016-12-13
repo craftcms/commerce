95b0afeeb8f651d486e597d654e6ea40a7c93284
Luke Holder <lukemh@gmail.com>
2016-12-13 10:18:05 +0800

    MultiSafepay_RestGatewayAdapter.php

--- src/Commerce/Gateways/Omnipay/MultiSafepay_RestGatewayAdapter.php
+++ src/Commerce/Gateways/Omnipay/MultiSafepay_RestGatewayAdapter.php
@@ -4,7 +4,7 @@ namespace Commerce\Gateways\Omnipay;
 use Craft\BaseModel;
 use Omnipay\Common\Message\AbstractRequest as OmnipayRequest;
 
-class MultiSafepay_RestGatewayAdapter extends \Commerce\Gateways\CreditCardGatewayAdapter
+class MultiSafepay_RestGatewayAdapter extends \Commerce\Gateways\OffsiteGatewayAdapter
 {
     public function handle()
     {
