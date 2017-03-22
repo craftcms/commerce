6a88b60932ce770f83a3aa99304d3aef491d1f00
Luke Holder <lukemh@gmail.com>
2017-03-22 15:01:24 +0800

    Move config and icons

--- src/config.php
+++ src/config.php
@@ -1,12 +1,12 @@
 <?php
 return [
-        'cartCookieDuration'               => 'P3M',
-        'paymentMethodSettings'            => [],
-        'purgeInactiveCarts'               => true,
-        'purgeInactiveCartsDuration'       => 'P3M',
-        'gatewayPostRedirectTemplate'      => '',
-        'sendCartInfoToGateways'           => true,
-        'requireEmailForAnonymousPayments' => false,
-        'useBillingAddressForTax'          => false,
-        'requireShippingAddressAtCheckout' => false
+    'cartCookieDuration' => 'P3M',
+    'paymentMethodSettings' => [],
+    'purgeInactiveCarts' => true,
+    'purgeInactiveCartsDuration' => 'P3M',
+    'gatewayPostRedirectTemplate' => '',
+    'sendCartInfoToGateways' => true,
+    'requireEmailForAnonymousPayments' => false,
+    'useBillingAddressForTax' => false,
+    'requireShippingAddressAtCheckout' => false
 ];
