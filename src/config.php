44d71c243121c0c419f88b5732af354239845cc9
brandonkelly <brandon@pixelandtonic.com>
2017-05-26 07:05:43 -0700

    format config.php

--- src/config.php
+++ src/config.php
@@ -1,13 +1,13 @@
 <?php
 return [
-        'cartCookieDuration'               => 'P3M',
-        'paymentMethodSettings'            => [],
-        'purgeInactiveCarts'               => true,
-        'purgeInactiveCartsDuration'       => 'P3M',
-        'gatewayPostRedirectTemplate'      => '',
-        'sendCartInfoToGateways'           => true,
-        'requireEmailForAnonymousPayments' => false,
-        'useBillingAddressForTax'          => false,
-        'requireShippingAddressAtCheckout' => false,
-        'requireBillingAddressAtCheckout' => false
+    'cartCookieDuration' => 'P3M',
+    'paymentMethodSettings' => [],
+    'purgeInactiveCarts' => true,
+    'purgeInactiveCartsDuration' => 'P3M',
+    'gatewayPostRedirectTemplate' => '',
+    'sendCartInfoToGateways' => true,
+    'requireEmailForAnonymousPayments' => false,
+    'useBillingAddressForTax' => false,
+    'requireShippingAddressAtCheckout' => false,
+    'requireBillingAddressAtCheckout' => false,
 ];
