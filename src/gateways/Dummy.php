12d8500f6e7d8d32394f9004d9ffa699219fe164
andris-sevcenko <asevcenko@gmail.com>
2017-08-09 15:31:11 +0300

    Fix dummy.

--- src/gateways/Dummy.php
+++ src/gateways/Dummy.php
@@ -66,12 +66,25 @@ class Dummy extends Gateway
     {
         return true;
     }
-
-
+    
     /**
      * @inheritdoc
      */
     protected function getRequest(Transaction $transaction, BasePaymentForm $form)
+    {
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function prepareCaptureRequest($request, string $reference)
+    {
+    }
+    
+    /**
+     * @inheritdoc
+     */
+    protected function prepareAuthorizeRequest($request)
     {
     }
 
@@ -85,7 +98,7 @@ class Dummy extends Gateway
     /**
      * @inheritdoc
      */
-    protected function prepareAuthorizeRequest($request)
+    protected function prepareRefundRequest($request, string $reference)
     {
     }
 
