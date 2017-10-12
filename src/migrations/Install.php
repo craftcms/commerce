462bdae3c693f5c5bac3998ebca7be17ac476baa
andris-sevcenko <asevcenko@gmail.com>
2017-10-12 12:03:17 +0300

    Fix install migration.

--- src/migrations/Install.php
+++ src/migrations/Install.php
@@ -82,6 +82,7 @@ class Install extends Migration
     {
         $this->createTable('{{%commerce_addresses}}', [
             'id' => $this->primaryKey(),
+            'stockLocation' => $this->boolean()->notNull()->defaultValue(false),
             'attention' => $this->string(),
             'title' => $this->string(),
             'firstName' => $this->string()->notNull(),
