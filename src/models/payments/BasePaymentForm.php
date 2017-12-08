09595b2d60b2851228612eaca65d76f74a64c160
andris-sevcenko <asevcenko@gmail.com>
2017-12-08 12:25:50 +0200

    Allow payment forms to be populated from payment source.

--- src/models/payments/BasePaymentForm.php
+++ src/models/payments/BasePaymentForm.php
@@ -3,6 +3,8 @@
 namespace craft\commerce\models\payments;
 
 use craft\base\Model;
+use craft\commerce\models\PaymentSource;
+use yii\base\NotSupportedException;
 
 /**
  * Class BasePaymentForm
@@ -12,4 +14,14 @@ use craft\base\Model;
  */
 abstract class BasePaymentForm extends Model
 {
+    /**
+     * Populate the payment form from a payment form.
+     *
+     * @param PaymentSource $paymentSource the source to ue
+     *
+     * @throws NotSupportedException if not supported by current gateway.
+     */
+    public function populateFromPaymentSource(PaymentSource $paymentSource) {
+        throw new NotSupportedException();
+    }
 }
