333cc3af64862534c30ce72891df4db9d6a5b017
andris-sevcenko <asevcenko@gmail.com>
2018-02-15 12:53:08 +0200

    Prettier detailed transaction information.

--- src/web/assets/commercecp/dist/js/TableRowAdditionalInfoIcon.js
+++ src/web/assets/commercecp/dist/js/TableRowAdditionalInfoIcon.js
@@ -18,7 +18,7 @@ Craft.Commerce.TableRowAdditionalInfoIcon = Garnish.Base.extend(
                 var item = this.$icon.closest('.infoRow');
                 var $hudBody = $("<div />");
                 var $title = $('<h2>Details</h2>').appendTo($hudBody);
-                var $table = $("<table class='data fullwidth'><tbody></tbody></table>").appendTo($hudBody);
+                var $table = $("<table class='data fullwidth detailHud'><tbody></tbody></table>").appendTo($hudBody);
                 var $tbody = $table.find('tbody');
 
                 var info = item.data('info');
@@ -26,7 +26,22 @@ Craft.Commerce.TableRowAdditionalInfoIcon = Garnish.Base.extend(
                 for (var i = 0; i < info.length; i++) {
                     var $tr = $('<tr />').appendTo($tbody);
                     var $label = $('<td><strong>' + Craft.t('commerce', info[i].label) + '</strong></td><td>').appendTo($tr);
-                    var $value = $('<td>' + info[i].value + '</td>').appendTo($tr);
+
+                    var value = info[i].value;
+                    switch (info[i].type) {
+                        case 'code':
+                            value = '<code>'+value+'</code>';
+                            break;
+                        case 'json':
+                            // Make sure we have proper spaces in it
+                            try {
+                                value = '<pre>'+syntaxHighlight(JSON.stringify(JSON.parse(value), undefined, 4))+'</pre>';
+                            } catch (e) {
+                            }
+                            value = '<div class="gateway-response">'+value+'</div>';
+                            break;
+                    }
+                    var $value = $('<td>' + value + '</td>').appendTo($tr);
                 }
 
                 this.hud = new Garnish.HUD(this.$icon, $hudBody, {
@@ -38,3 +53,23 @@ Craft.Commerce.TableRowAdditionalInfoIcon = Garnish.Base.extend(
             }
         }
     });
+
+// Borrowed from https://stackoverflow.com/a/7220510/2040791
+function syntaxHighlight(json) {
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
