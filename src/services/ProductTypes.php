a0cace5710c15a6073d1726e2d33bbf89c19ca4b
andris-sevcenko <asevcenko@gmail.com>
2018-03-03 11:20:26 +0000

    Fire the event before validating.

--- src/services/ProductTypes.php
+++ src/services/ProductTypes.php
@@ -259,12 +259,6 @@ class ProductTypes extends Component
     {
         $titleFormatChanged = false;
 
-        if ($runValidation && !$productType->validate()) {
-            Craft::info('Product type not saved due to validation error.', __METHOD__);
-
-            return false;
-        }
-
         $isNewProductType = !$productType->id;
 
         // Fire a 'beforeSaveProductType' event
@@ -273,6 +267,12 @@ class ProductTypes extends Component
                 'productType' => $productType,
                 'isNew' => $isNewProductType,
             ]));
+        }
+
+        if ($runValidation && !$productType->validate()) {
+            Craft::info('Product type not saved due to validation error.', __METHOD__);
+
+            return false;
         }
 
         if (!$isNewProductType) {
