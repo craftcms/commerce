8fbf04ad7fee1e64eefc221efd89f4f1c479c3b6
brandonkelly <brandon@pixelandtonic.com>
2018-03-19 10:51:58 -0700

    Fixed extra space in order status labels

--- src/models/OrderStatus.php
+++ src/models/OrderStatus.php
@@ -108,6 +108,6 @@ class OrderStatus extends Model
      */
     public function getLabelHtml(): string
     {
-        return sprintf('<span class="commerceStatusLabel"><span class="status %s"></span> %s</span>', $this->color, $this->name);
+        return sprintf('<span class="commerceStatusLabel"><span class="status %s"></span>%s</span>', $this->color, $this->name);
     }
 }
