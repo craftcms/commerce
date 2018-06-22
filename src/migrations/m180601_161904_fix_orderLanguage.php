cdaf0d8e6c4e76a5e4ca136a1e10ff07e7f3aaa5
Luke Holder <lukemh@gmail.com>
2018-06-22 16:13:57 +0800

    Fix migration on psql

--- src/migrations/m180601_161904_fix_orderLanguage.php
+++ src/migrations/m180601_161904_fix_orderLanguage.php
@@ -22,14 +22,14 @@ class m180601_161904_fix_orderLanguage extends Migration
         if ($this->db->getIsPgsql()) {
             // Manually construct the SQL for Postgres
             // (see https://github.com/yiisoft/yii2/issues/12077)
-            $this->execute('alter table {{%commerce_orders}} alter column [[handle]] type varchar(12), alter column [[orderLocale]] set not null');
+            $this->execute('alter table {{%commerce_orders}} alter column [[orderLocale]] type varchar(12), alter column [[orderLocale]] set not null');
         } else {
             $this->alterColumn('{{%commerce_orders}}', 'orderLocale', $this->string(12)->notNull());
         }
 
         $this->renameColumn('{{%commerce_orders}}', 'orderLocale', 'orderLanguage');
 
-        $sql = 'update {{%commerce_orders}} [[o]] set [[o.orderLanguage]] = TRIM([[o.orderLanguage]])';
+        $sql = 'update {{%commerce_orders}} set [[orderLanguage]] = TRIM([[orderLanguage]])';
 
         $this->execute($sql);
 
