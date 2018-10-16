4b47fff399e80e49e2d06447e2f315761e8e038d
brandonkelly <brandon@pixelandtonic.com>
2018-10-16 16:39:51 -0700

    is() operator is '=' by default now

--- src/elements/Order.php
+++ src/elements/Order.php
@@ -714,7 +714,7 @@ class Order extends Element
             return;
         }
 
-        if (!Plugin::getInstance()->is(Plugin::EDITION_STANDARD)) {
+        if (Plugin::getInstance()->is(Plugin::EDITION_LITE)) {
             $this->setAdjustments([]);
             return;
         }
