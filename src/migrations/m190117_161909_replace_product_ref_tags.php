27e3cf3da5018ea59c2c6220a513536cc26e938d
Luke Holder <lukemh@gmail.com>
2019-01-18 02:46:05 +0800

    Fixed #627

---
+++ src/migrations/m190117_161909_replace_product_ref_tags.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @link https://craftcms.com/
+ * @copyright Copyright (c) Pixel & Tonic, Inc.
+ * @license https://craftcms.github.io/license/
+ */
+
+namespace craft\commerce\migrations;
+
+use craft\db\Migration;
+use craft\queue\jobs\FindAndReplace;
+
+/**
+ * m190117_161909_replace_product_ref_tags migration.
+ */
+class m190117_161909_replace_product_ref_tags extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        \Craft::$app->getQueue()->push(new FindAndReplace([
+            'find' => '{commerce_product:',
+            'replace' => '{product:',
+        ]));
+
+        \Craft::$app->getQueue()->push(new FindAndReplace([
+            'find' => '{commerce_variant:',
+            'replace' => '{variant:',
+        ]));
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        echo "m190117_161909_replace_product_ref_tags cannot be reverted.\n";
+        return false;
+    }
+}
