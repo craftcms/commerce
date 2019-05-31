fa64c265379ded256e2ed519531c6756d327459a
Benjamin David <ben@dukt.net>
2019-05-31 21:16:26 +0200

    Added LineItemStatus component

--- src/web/assets/commerceui/src/js/components/LineItem.vue
+++ src/web/assets/commerceui/src/js/components/LineItem.vue
@@ -13,38 +13,7 @@
                             <h3>{{ lineItem.description }}</h3>
 
                             <!-- Status -->
-                            <div>
-                                <a class="btn menubtn" ref="lineItemStatus">
-                                    <template v-if="lineItemStatus.color">
-                                        <span class="status" :class="{[lineItemStatus.color]: true}"></span>
-                                    </template>
-                                    <template v-else>
-                                        <span class="status"></span>
-                                    </template>
-
-                                    {{lineItemStatus.name}}
-                                </a>
-                                <div class="menu">
-                                    <ul class="padded" role="listbox">
-                                        <li>
-                                            <a data-id="0" data-name="None">
-                                                <span class="status"></span>
-                                                None
-                                            </a>
-                                        </li>
-                                        <li v-for="(status) in $root.lineItemStatuses">
-                                            <a
-                                                    :data-id="status.id"
-                                                    :data-color="status.color"
-                                                    :data-name="status.name"
-                                                    :class="{sel: lineItemStatus.id === status.value}">
-                                                <span class="status" :class="{[status.color]: true}"></span>
-                                                {{status.name}}
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
+                            <line-item-status :line-item="lineItem"></line-item-status>
 
                             <br />
 
@@ -215,12 +184,14 @@
     import PrismEditor from 'vue-prism-editor'
     import Adjustments from './Adjustments'
     import InputError from './InputError'
+    import LineItemStatus from './LineItemStatus'
 
     export default {
         components: {
             PrismEditor,
             Adjustments,
             InputError,
+            LineItemStatus,
         },
 
         props: {
@@ -240,20 +211,6 @@
         },
 
         computed: {
-            lineItemStatus() {
-                if (this.lineItem.lineItemStatusId !== '0') {
-                    for (let lineItemStatusesKey in this.lineItemStatuses) {
-                        const lineItemStatus = this.lineItemStatuses[lineItemStatusesKey]
-
-                        if (lineItemStatus.id === this.lineItem.lineItemStatusId) {
-                            return lineItemStatus
-                        }
-                    }
-                }
-
-                return {id: "0", name: "None", color: null}
-            },
-
             shippingCategory() {
                 if (!this.lineItem.shippingCategoryId) {
                     return null
@@ -285,16 +242,6 @@
                 this.$emit('change')
             },
 
-            onSelectStatus(status) {
-                if (status.dataset.id === '0') {
-                    this.lineItem.lineItemStatusId = null
-                } else {
-                    this.lineItem.lineItemStatusId = status.dataset.id
-                }
-
-                this.$emit('change')
-            },
-
             onChange() {
                 this.$emit('change')
             },
@@ -311,10 +258,6 @@
 
             this.onChange = debounce(this.onChange, 1000)
             this.onOptionsChange = debounce(this.onOptionsChange, 1000)
-
-            new Garnish.MenuBtn(this.$refs.lineItemStatus, {
-                onOptionSelect: this.onSelectStatus
-            })
         },
     }
 </script>
---
+++ src/web/assets/commerceui/src/js/components/LineItemStatus.vue
@@ -0,0 +1,79 @@
+<template>
+    <div>
+        <a class="btn menubtn" ref="lineItemStatus">
+            <template v-if="lineItemStatus.color">
+                <span class="status" :class="{[lineItemStatus.color]: true}"></span>
+            </template>
+            <template v-else>
+                <span class="status"></span>
+            </template>
+
+            {{lineItemStatus.name}}
+        </a>
+        <div class="menu">
+            <ul class="padded" role="listbox">
+                <li>
+                    <a data-id="0" data-name="None">
+                        <span class="status"></span>
+                        None
+                    </a>
+                </li>
+                <li v-for="(status) in $root.lineItemStatuses">
+                    <a
+                            :data-id="status.id"
+                            :data-color="status.color"
+                            :data-name="status.name"
+                            :class="{sel: lineItemStatus.id === status.value}">
+                        <span class="status" :class="{[status.color]: true}"></span>
+                        {{status.name}}
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+        props: {
+            lineItem: {
+                type: Object,
+            },
+        },
+
+        computed: {
+            lineItemStatus() {
+                if (this.lineItem.lineItemStatusId !== '0') {
+                    for (let lineItemStatusesKey in this.$root.lineItemStatuses) {
+                        const lineItemStatus = this.$root.lineItemStatuses[lineItemStatusesKey]
+
+                        if (lineItemStatus.id === this.lineItem.lineItemStatusId) {
+                            return lineItemStatus
+                        }
+                    }
+                }
+
+                return {id: "0", name: "None", color: null}
+            },
+        },
+
+        methods: {
+            onSelectStatus(status) {
+                if (status.dataset.id === '0') {
+                    this.lineItem.lineItemStatusId = null
+                } else {
+                    this.lineItem.lineItemStatusId = status.dataset.id
+                }
+                
+                this.$emit('change')
+            },
+        },
+
+        mounted() {
+            new Garnish.MenuBtn(this.$refs.lineItemStatus, {
+                onOptionSelect: this.onSelectStatus
+            })
+        }
+    }
+</script>
