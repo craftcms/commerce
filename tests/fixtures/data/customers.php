3ba5cf19c1ac1a43c79ee87e4c56f6803c445b88
lemiwinkz <giel@yellowflash.net>
2019-06-01 14:31:19 +0200

    Fixtures

---
+++ tests/fixtures/data/customers.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @link https://craftcms.com/
+ * @copyright Copyright (c) Pixel & Tonic, Inc.
+ * @license https://craftcms.github.io/license/
+ */
+
+return [
+    [
+        'id' => '1000',
+        'userId' => '1',
+    ]
+];
---
+++ tests/fixtures/data/discounts.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @link https://craftcms.com/
+ * @copyright Copyright (c) Pixel & Tonic, Inc.
+ * @license https://craftcms.github.io/license/
+ */
+
+return [
+    [
+        'name' => 'Discount 1',
+        'code' => 'discount_1',
+        'perUserLimit' => '1',
+        'totalUseLimit' => '2',
+        'baseDiscount' => '10',
+        'perItemDiscount' => '5',
+        'percentDiscount' => '15.25',
+        'enabled' => true,
+        'allGroups' => true,
+    ]
+];
