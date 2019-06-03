d6efea0516e5938171de4323d75622f8efb293fa
Benjamin David <ben@dukt.net>
2019-06-03 12:13:59 +0200

    Rebuild draft data from scratch when recalculating

--- src/web/assets/commerceui/src/js/order-details.js
+++ src/web/assets/commerceui/src/js/order-details.js
@@ -126,38 +126,60 @@ new Vue({
             this.recalculateLoading = true
 
 
-            // make sure values have the right type
+            // build draft data to be sent and make sure values have the right type
 
-            draft.order.id = this.parseInputValue('int', draft.order.id)
+            const draftData = {
+                order: {
+                    recalculationMode: draft.order.recalculationMode,
+                    lineItems: [],
+                    orderAdjustments: [],
+                }
+            }
+
+            draftData.order.id = this.parseInputValue('int', draft.order.id)
 
             draft.order.lineItems.forEach((lineItem, lineItemKey) => {
-                draft.order.lineItems[lineItemKey].lineItemStatusId = this.parseInputValue('int', lineItem.lineItemStatusId)
-                draft.order.lineItems[lineItemKey].id = this.parseInputValue('int', lineItem.id)
-                draft.order.lineItems[lineItemKey].purchasableId = this.parseInputValue('int', lineItem.purchasableId)
-                draft.order.lineItems[lineItemKey].shippingCategoryId = this.parseInputValue('int', lineItem.shippingCategoryId)
-                draft.order.lineItems[lineItemKey].salePrice = this.parseInputValue('float', lineItem.salePrice)
-                draft.order.lineItems[lineItemKey].qty = this.parseInputValue('int', lineItem.qty)
+                draftData.order.lineItems[lineItemKey] = {}
+                draftData.order.lineItems[lineItemKey].lineItemStatusId = this.parseInputValue('int', lineItem.lineItemStatusId)
+                draftData.order.lineItems[lineItemKey].id = this.parseInputValue('int', lineItem.id)
+                draftData.order.lineItems[lineItemKey].purchasableId = this.parseInputValue('int', lineItem.purchasableId)
+                draftData.order.lineItems[lineItemKey].shippingCategoryId = this.parseInputValue('int', lineItem.shippingCategoryId)
+                draftData.order.lineItems[lineItemKey].salePrice = this.parseInputValue('float', lineItem.salePrice)
+                draftData.order.lineItems[lineItemKey].qty = this.parseInputValue('int', lineItem.qty)
+                draftData.order.lineItems[lineItemKey].note = lineItem.note
+                draftData.order.lineItems[lineItemKey].adminNote = lineItem.adminNote
+                draftData.order.lineItems[lineItemKey].orderId = lineItem.orderId
+                draftData.order.lineItems[lineItemKey].options = lineItem.options
+                draftData.order.lineItems[lineItemKey].adjustments = []
 
                 lineItem.adjustments.forEach((adjustment, adjustmentKey) => {
-                    draft.order.lineItems[lineItemKey].adjustments[adjustmentKey].id = this.parseInputValue('int', adjustment.id)
-                    draft.order.lineItems[lineItemKey].adjustments[adjustmentKey].amount = this.parseInputValue('float', adjustment.amount)
-                    draft.order.lineItems[lineItemKey].adjustments[adjustmentKey].included = this.parseInputValue('bool', adjustment.included)
-                    draft.order.lineItems[lineItemKey].adjustments[adjustmentKey].orderId = this.parseInputValue('int', adjustment.orderId)
-                    draft.order.lineItems[lineItemKey].adjustments[adjustmentKey].lineItemId = this.parseInputValue('int', adjustment.lineItemId)
+                    draftData.order.lineItems[lineItemKey].adjustments[adjustmentKey] = {}
+                    draftData.order.lineItems[lineItemKey].adjustments[adjustmentKey].id = this.parseInputValue('int', adjustment.id)
+                    draftData.order.lineItems[lineItemKey].adjustments[adjustmentKey].amount = this.parseInputValue('float', adjustment.amount)
+                    draftData.order.lineItems[lineItemKey].adjustments[adjustmentKey].included = this.parseInputValue('bool', adjustment.included)
+                    draftData.order.lineItems[lineItemKey].adjustments[adjustmentKey].orderId = this.parseInputValue('int', adjustment.orderId)
+                    draftData.order.lineItems[lineItemKey].adjustments[adjustmentKey].lineItemId = this.parseInputValue('int', adjustment.lineItemId)
+                    draftData.order.lineItems[lineItemKey].adjustments[adjustmentKey].name = adjustment.name
+                    draftData.order.lineItems[lineItemKey].adjustments[adjustmentKey].description = adjustment.description
+                    draftData.order.lineItems[lineItemKey].adjustments[adjustmentKey].type = adjustment.type
                 })
             })
 
             draft.order.orderAdjustments.forEach((adjustment, adjustmentKey) => {
-                draft.order.orderAdjustments[adjustmentKey].id = this.parseInputValue('int', adjustment.id)
-                draft.order.orderAdjustments[adjustmentKey].amount = this.parseInputValue('float', adjustment.amount)
-                draft.order.orderAdjustments[adjustmentKey].included = this.parseInputValue('bool', adjustment.included)
-                draft.order.orderAdjustments[adjustmentKey].orderId = this.parseInputValue('int', adjustment.orderId)
+                draftData.order.orderAdjustments[adjustmentKey] = {}
+                draftData.order.orderAdjustments[adjustmentKey].id = this.parseInputValue('int', adjustment.id)
+                draftData.order.orderAdjustments[adjustmentKey].amount = this.parseInputValue('float', adjustment.amount)
+                draftData.order.orderAdjustments[adjustmentKey].included = this.parseInputValue('bool', adjustment.included)
+                draftData.order.orderAdjustments[adjustmentKey].orderId = this.parseInputValue('int', adjustment.orderId)
+                draftData.order.orderAdjustments[adjustmentKey].name = adjustment.name
+                draftData.order.orderAdjustments[adjustmentKey].description = adjustment.description
+                draftData.order.orderAdjustments[adjustmentKey].type = adjustment.type
             })
 
 
-            // recalculate
+            // Recalculate
 
-            orderApi.recalculate(draft)
+            orderApi.recalculate(draftData)
                 .then((response) => {
                     this.recalculateLoading = false
                     this.draft = JSON.parse(JSON.stringify(response.data))
