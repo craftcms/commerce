2642980b03810284951780ef6cde1bc41d98b92f
Benjamin David <ben@dukt.net>
2019-06-04 19:47:19 +0200

    Clean up

--- src/web/assets/commerceui/src/js/app.js
+++ src/web/assets/commerceui/src/js/app.js
@@ -149,8 +149,6 @@ window.OrderDetailsApp = new Vue({
                     if (!this.originalDraft) {
                         this.originalDraft = JSON.parse(JSON.stringify(this.draft))
                     }
-
-                    EventBus.$emit('afterGetOrder', this.draft)
                 })
                 .catch((error) => {
                     this.recalculateLoading = false
@@ -311,18 +309,6 @@ window.OrderDetailsApp = new Vue({
 window.OrderMetaApp = new Vue({
     render: h => h(OrderMeta),
 
-    // data() {
-    //     return {
-    //         draft: null,
-    //     }
-    // },
-    //
-    // watch: {
-    //     draft(newVal) {
-    //         window.OrderDetailsApp.draft = JSON.parse(JSON.stringify(newVal))
-    //     }
-    // },
-
     computed: {
         draft: {
             get() {
@@ -339,14 +325,4 @@ window.OrderMetaApp = new Vue({
             return window.OrderDetailsApp.save()
         }
     }
-
-    // mounted() {
-    //     // Get a copy of the initial draft from the order details app
-    //     this.draft = window.OrderDetailsApp.draft
-    //
-    //     // Update local draft when draft changes in the order details app
-    //     EventBus.$on('afterGetOrder', (draft) => {
-    //         this.draft = JSON.parse(JSON.stringify(draft))
-    //     });
-    // }
 }).$mount('#order-meta-app')
