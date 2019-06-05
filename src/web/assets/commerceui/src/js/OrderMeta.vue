d0c6acad7e2ae05e93aff2fa03c468bf96697360
Luke Holder <lukemh@gmail.com>
2019-06-05 19:21:12 +0800

    inCompleted checkbox

--- src/web/assets/commerceui/src/js/OrderMeta.vue
+++ src/web/assets/commerceui/src/js/OrderMeta.vue
@@ -38,18 +38,16 @@
                 </div>
             </div>
 
-            <template v-if="!$root.draft.order.isCompleted">
-                <div class="field" id="isCompleted-field">
-                    <div class="heading">
-                        <label id="isCompleted-label" for="slug">Status</label>
-                    </div>
-                    <div class="input ltr">
-                        <div class="buttons">
-                            <input type="button" class="btn" value="Complete Order" />
-                        </div>
+            <div class="field" id="isCompleted-field">
+                <div class="heading">
+                    <label id="isCompleted-label" for="slug">Completed</label>
+                </div>
+                <div class="input ltr">
+                    <div class="buttons">
+                        <input type="checkbox" :disabled="$root.draft.order.isCompleted" v-model="$root.draft.order.isCompleted">
                     </div>
                 </div>
-            </template>
+            </div>
 
             <template v-if="$root.draft.order.isCompleted">
             <div class="field" id="orderStatus-field">
