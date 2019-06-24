a643e8699dc9920b52ec1dda3055881768e8c7b7
Luke Holder <lukemh@gmail.com>
2019-06-24 15:59:21 +0800

    Single orders API

--- src/web/assets/commerceui/src/js/api/orders.js
+++ src/web/assets/commerceui/src/js/api/orders.js
@@ -3,6 +3,68 @@
 import axios from 'axios/index'
 
 export default {
+    get(orderId, checkPage) {
+        // If we have the order loaded into the page already return that data and save us a ajax trip
+        if(checkPage && window.orderEdit.data) {
+            return new Promise((resolve) => {
+                var response = {}
+                response.data = window.orderEdit.data
+                resolve(response)
+            });
+        }
+
+        return axios.get(Craft.getActionUrl('commerce/orders/get', {orderId}), {
+            headers: {
+                'X-CSRF-Token':  Craft.csrfTokenValue,
+            }
+        })
+    },
+
+    recalculate(draft) {
+        return axios.post(Craft.getActionUrl('commerce/orders/refresh'), draft, {
+            headers: {
+                'X-CSRF-Token':  Craft.csrfTokenValue,
+            }
+        })
+    },
+
+    purchasableSearch(orderId, query) {
+        const data = {
+            orderId
+        }
+
+        if (typeof query !== 'undefined') {
+            data.query = query
+        }
+
+        return axios.get(Craft.getActionUrl('commerce/orders/purchasable-search', data), {
+            headers: {
+                [Craft.csrfTokenName]:  Craft.csrfTokenValue,
+            }
+        })
+    },
+
+    customerSearch(query) {
+        const data = {}
+
+        if (typeof query !== 'undefined') {
+            data.query = query
+        }
+
+        return axios.get(Craft.getActionUrl('commerce/orders/customer-search', data), {
+            headers: {
+                [Craft.csrfTokenName]:  Craft.csrfTokenValue,
+            }
+        })
+    },
+
+    sendEmail(emailTemplateId) {
+        return axios.post(Craft.getActionUrl('commerce/orders/send-email', {id: emailTemplateId}), {}, {
+            headers: {
+                'X-CSRF-Token':  Craft.csrfTokenValue,
+            }
+        })
+    },
     deleteOrder(orderId) {
         let formData = new FormData();
         formData.append('orderId', orderId)
