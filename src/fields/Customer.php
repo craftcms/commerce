fc54ebb7f5c87e3e9a09dd295d43e5042469493d
Luke Holder <lukemh@gmail.com>
2019-10-21 12:20:19 -0700

    Cleanup - Clarity on version number

--- src/fields/Customer.php
+++ src/fields/Customer.php
@@ -47,7 +47,7 @@ class Customer extends Field
      */
     public function getInputHtml($value, ElementInterface $element = null): string
     {
-        Craft::$app->getDeprecator()->log('commerceCustomerInfoField', 'The Commerce Customer Info custom field will be removed in Commerce 3.0.');
+        Craft::$app->getDeprecator()->log('commerceCustomerInfoField', 'The Commerce Customer Info custom field will be removed in Commerce 3.0');
 
         if (!($element instanceof User)) {
             return '<span style="color: #da5a47">' . Craft::t('commerce', 'Commerce Customer Info field is for user profiles only.') . '</span>';
