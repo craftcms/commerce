c128c0cbbfe116be45d12fc16a23fc2788fdda8b
Nathaniel Hammond <nathanielhammond@gmail.com>
2019-10-22 10:22:09 -0700

    Added migration for isEstimated in order adjustments

---
+++ src/migrations/m191022_162628_add_estimated_toadjustments.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace craft\commerce\migrations;
+
+use Craft;
+use craft\db\Migration;
+
+/**
+ * m191022_162628_add_estimated_toadjustments migration.
+ */
+class m191022_162628_add_estimated_toadjustments extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        $this->addColumn('{{%commerce_orderadjustments}}', 'isEstimated', $this->boolean()->notNull()->defaultValue(false));
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        echo "m191022_162628_add_estimated_toadjustments cannot be reverted.\n";
+        return false;
+    }
+}
