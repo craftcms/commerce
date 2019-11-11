c9af08d87053b7a1203041f140b9516c4afe5740
Nathaniel Hammond <nathanielhammond@gmail.com>
2019-11-11 14:53:26 +0000

    Updated migrations for postgres

--- src/migrations/m191008_153947_add_relationship_type_to_discounts.php
+++ src/migrations/m191008_153947_add_relationship_type_to_discounts.php
@@ -18,7 +18,20 @@ class m191008_153947_add_relationship_type_to_discounts extends Migration
         $columnName = 'categoryRelationshipType';
         $values = ['sourceElement', 'targetElement', 'element'];
 
-        $this->addColumn('{{%commerce_discounts}}', $columnName, $this->enum($columnName, $values)->after('allCategories')->notNull()->defaultValue('sourceElement'));
+        if ($this->db->getIsPgsql()) {
+            // Manually construct the SQL for Postgres
+            $check = '[['.$columnName.']] in (';
+            foreach ($values as $i => $value) {
+                if ($i != 0) {
+                    $check .= ',';
+                }
+                $check .= $this->db->quoteValue($value);
+            }
+            $check .= ')';
+            $this->execute("alter table {{%commerce_discounts}} drop constraint {{%commerce_discounts_".$columnName."_check}}, add check ({$check})");
+        } else {
+            $this->addColumn('{{%commerce_discounts}}', $columnName, $this->enum($columnName, $values)->notNull()->defaultValue('element'));
+        }
     }
 
     /**
--- src/migrations/m191008_155732_add_relationship_type_to_sales.php
+++ src/migrations/m191008_155732_add_relationship_type_to_sales.php
@@ -18,7 +18,20 @@ class m191008_155732_add_relationship_type_to_sales extends Migration
         $columnName = 'categoryRelationshipType';
         $values = ['sourceElement', 'targetElement', 'element'];
 
-        $this->addColumn('{{%commerce_sales}}', $columnName, $this->enum($columnName, $values)->after('allCategories')->notNull()->defaultValue('sourceElement'));
+        if ($this->db->getIsPgsql()) {
+            // Manually construct the SQL for Postgres
+            $check = '[['.$columnName.']] in (';
+            foreach ($values as $i => $value) {
+                if ($i != 0) {
+                    $check .= ',';
+                }
+                $check .= $this->db->quoteValue($value);
+            }
+            $check .= ')';
+            $this->execute("alter table {{%commerce_sales}} drop constraint {{%commerce_sales_".$columnName."_check}}, add check ({$check})");
+        } else {
+            $this->addColumn('{{%commerce_sales}}', $columnName, $this->enum($columnName, $values)->notNull()->defaultValue('element'));
+        }
     }
 
     /**
