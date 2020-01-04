7be99917e53af8ac8be0fc07f509932c3a02de55
Luke Holder <lukemh@gmail.com>
2020-01-03 21:08:25 -0800

    Try to remove orphans before running next migration

---
+++ src/migrations/m200101_114320_remove_orphan_customers.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace craft\commerce\migrations;
+
+use Craft;
+use craft\db\Migration;
+use craft\db\Query;
+
+/**
+ * m200102_114323_update_order_customer_ids migration.
+ */
+class m200101_114320_remove_orphan_customers extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        $customers = (new Query())
+            ->select(['[[customers.id]] id'])
+            ->from('{{%commerce_customers}} customers')
+            ->leftJoin('{{%commerce_orders}} orders', '[[customers.id]] = [[orders.customerId]]')
+            ->where(['[[orders.customerId]]' => null, '[[customers.userId]]' => null])
+            ->column();
+
+        // This will also remove all addresses related to the customer.
+        Craft::$app->getDb()->createCommand()
+            ->delete('{{%commerce_customers}}', ['id' => $customers])
+            ->execute();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        echo "m200102_114323_update_order_customer_ids cannot be reverted.\n";
+        return false;
+    }
+}
--- src/migrations/m200102_114323_update_order_customer_ids.php
+++ src/migrations/m200102_114323_update_order_customer_ids.php
@@ -18,18 +18,6 @@ class m200102_114323_update_order_customer_ids extends Migration
      */
     public function safeUp()
     {
-        $customers = (new Query())
-            ->select(['[[customers.id]] id'])
-            ->from('{{%commerce_customers}} customers')
-            ->leftJoin('{{%commerce_orders}} orders', '[[customers.id]] = [[orders.customerId]]')
-            ->where(['[[orders.customerId]]' => null, '[[customers.userId]]' => null])
-            ->column();
-
-        // This will also remove all addresses related to the customer.
-        Craft::$app->getDb()->createCommand()
-            ->delete('{{%commerce_customers}}', ['id' => $customers])
-            ->execute();
-
         $allCustomers = (new Query())
             ->select('[[orders.email]], [[orders.customerId]]')
             ->from('{{%commerce_orders}} orders')
