f97f1b763121e93ec0a7ea0c196587b1be6bd4ba
Luke Holder <lukemh@gmail.com>
2020-01-04 14:08:00 -0800

    optimize

--- src/queue/ConsolidateGuestOrders.php
+++ src/queue/ConsolidateGuestOrders.php
@@ -33,39 +33,45 @@ class ConsolidateGuestOrders extends BaseJob
             ->select('[[orders.customerId]]')
             ->from(Table::ORDERS . ' orders')
             ->innerJoin(Table::CUSTOMERS . ' customers', '[[customers.id]] = [[orders.customerId]]')
-            ->where(['email' => $this->email])
-            ->andWhere(['isCompleted' => 1])
-            // we want the customers related to a userId to be listed first, then by latest order
+            ->where(['[[orders.email]]' => $this->email])
+            ->andWhere(['[[orders.isCompleted]]' => true])
+            // we want the customers related to a userId to be listed first, then by their latest order
             ->orderBy('[[customers.userId]] DESC, [[orders.dateOrdered]] ASC')
-            ->scalar();
+            ->scalar(); // get the first customerId in the result
 
         if (!$customerId) {
             return;
         }
 
-        // Get completed orders for other customers with the same email
-        $orders = Order::find()
-            ->isCompleted(1)
-            ->customerId('not ' . $customerId)
-            ->email($this->email)
+        // Get completed orders for other customers with the same email but not the same customer
+        $orders = (new Query())
+            ->select(['[[orders.id]] id ', '[[customers.userId]] userId'])
+            ->where(['and', ['[[orders.email]]' => $this->email, '[[orders.isCompleted]]' => true], ['not', ['[[orders.customerId]]' => $customerId]]])
+            ->leftJoin(Table::CUSTOMERS . ' customers', '[[orders.customerId]] = [[customers.id]]')
+            ->from(Table::ORDERS . ' orders')
             ->all();
 
         $total = count($orders) + 1;
         $step = 1;
 
         foreach ($orders as $order) {
+
+            $userId = $order['userId'];
+            $orderId = $order['id'];
+
             $this->setProgress($queue, $step / $total, Plugin::t('Order {step} of {total}', compact('step', 'total')));
 
-            $belongsToAnotherUser = $order->getCustomer() && $order->getCustomer()->getUser();
-
-            if (!$belongsToAnotherUser) {
+            if (!$userId) {
                 // Dont use element save, just update DB directly
-                Craft::$app->getDb()->createCommand()->update('{{%commerce_orders}} orders', ['customerId' => $customerId], ['[[orders.id]]' => $order->id]);
+                Craft::$app->getDb()->createCommand()
+                    ->update('{{%commerce_orders}} orders', ['[[orders.customerId]]' => $customerId], ['[[orders.id]]' => $orderId])
+                    ->execute();
             }
 
             $step++;
         }
 
+        // Now that we have a much of customers that no longer have an order, clean up the orphaned customers.
         $this->setProgress($queue, $step / $total, Plugin::t('Purging orphaned customers.'));
         Plugin::getInstance()->getCustomers()->purgeOrphanedCustomers();
     }
