ba75182769a795098c81fe47c378bb3f6f43d07d
Nathaniel Hammond <nathanielhammond@gmail.com>
2020-01-08 13:09:24 +0000

    Consolidate guets orders migration

---
+++ src/migrations/m200108_114623_consolidate_customer_records.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace craft\commerce\migrations;
+
+use Craft;
+use craft\commerce\queue\ConsolidateGuestOrders;
+use craft\db\Migration;
+use craft\db\Query;
+use yii\db\Expression;
+
+/**
+ * m200108_114623_consolidate_customer_records migration.
+ */
+class m200108_114623_consolidate_customer_records extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        $customers = (new Query())
+            ->select([
+                'email',
+                new Expression('COUNT(DISTINCT [[customerId]]) as customerIdCount')
+            ])
+            ->from('{{%commerce_orders}}')
+            ->where(['isCompleted' => true])
+            ->groupBy('[[email]]')
+            ->orderBy(new Expression('COUNT(DISTINCT [[customerId]]) DESC'))
+            ->all();
+
+        if (!empty($customers)) {
+            $consolidateCustomers = array_filter($customers, static function($customer){
+                return $customer['customerIdCount'] > 1;
+            });
+
+            if (!empty($consolidateCustomers)) {
+                foreach ($consolidateCustomers as $consolidateCustomer) {
+                    Craft::$app->getQueue()->push(new ConsolidateGuestOrders([
+                        'emails' => [$consolidateCustomer['email']]
+                    ]));
+                }
+            }
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        echo "m200108_114623_consolidate_customer_records cannot be reverted.\n";
+        return false;
+    }
+}
