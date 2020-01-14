3a381a7263813fa347dd9e9019a82092bffa2d0b
Nathaniel Hammond <nathanielhammond@gmail.com>
2020-01-14 14:21:12 +0000

    New Customers stat

---
+++ src/stats/NewCustomers.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @link https://craftcms.com/
+ * @copyright Copyright (c) Pixel & Tonic, Inc.
+ * @license https://craftcms.github.io/license/
+ */
+
+namespace craft\commerce\stats;
+
+use craft\commerce\base\Stat;
+use yii\db\Expression;
+
+/**
+ * Total Orders Stat
+ *
+ * @author Pixel & Tonic, Inc. <support@pixelandtonic.com>
+ * @since 3.0
+ */
+class NewCustomers extends Stat
+{
+    // Properties
+    // =========================================================================
+
+    /**
+     * @inheritdoc
+     */
+    protected $_handle = 'newCustomers';
+
+    // Public Methods
+    // =========================================================================
+
+    /**
+     * @inheritDoc
+     */
+    public function getData()
+    {
+        $query = $this->_createStatQuery();
+        $query->select([new Expression('COUNT(DISTINCT [[customerId]]) as newCustomers')]);
+
+        return $query->scalar();
+    }
+}
