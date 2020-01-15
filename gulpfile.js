441cfc399f0ff893ef3b41cde9cd8f3e75b9a252
Nathaniel Hammond <nathanielhammond@gmail.com>
2020-01-15 16:02:59 +0000

    Include chart.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 var gulp = require('gulp');
+var es = require('event-stream');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
-
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
 sass.compiler = require('node-sass');
 
 var assetsPath = './src/web/assets/';
+
+var jsDeps = [
+    { srcGlob: 'node_modules/chart.js/dist/Chart.js', dest: assetsPath + '/statwidgets/dist/js/chart-js' },
+];
 
 function commerceSass () {
     return gulp.src(assetsPath+'commercecp/src/scss/**/*.scss')
@@ -19,7 +25,28 @@ function commerceJs() {
         .pipe(gulp.dest(assetsPath+'commercecp/dist/js'));
 };
 
+function libDeps() {
+    var streams = [];
+
+    // Minify & move the JS deps
+    jsDeps.forEach(function(dep) {
+        streams.push(
+            gulp.src(dep.srcGlob)
+                //.pipe(gulp.dest(dest))
+                .pipe(sourcemaps.init())
+                .pipe(uglify())
+                //.pipe(rename({ suffix: '.min' }))
+                .pipe(sourcemaps.write('./'))
+                .pipe(gulp.dest(dep.dest))
+        );
+    });
+
+    return es.merge(streams);
+};
+
+
 exports.default = function(done) {
+    libDeps();
     gulp.series(commerceJs, commerceSass)(done);
 }
 
