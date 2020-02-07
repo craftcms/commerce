f5c01440bc281b571b6120eaa896b27f3a3d38a2
Nathaniel Hammond <nathanielhammond@gmail.com>
2020-02-07 17:33:24 +0000

    Fixed linting issue

--- src/web/assets/commerceui/src/js/components/details/LineItems.vue
+++ src/web/assets/commerceui/src/js/components/details/LineItems.vue
@@ -9,7 +9,7 @@
                     :editing="editing"
                     @updateLineItem="updateLineItem($event, lineItemKey)"
                     @removeLineItem="removeLineItem(lineItemKey)"></line-item>
-            <hr v-if="editing || lineItemKey != (lineItems.length -1)">
+            <hr v-bind:key="lineItemKey" v-if="editing || lineItemKey != (lineItems.length -1)">
         </template>
     </div>
 </template>
