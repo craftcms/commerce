c9666146d1dabaef2fcd2e6f478e6909b6b63238
Nathaniel Hammond <nathanielhammond@gmail.com>
2020-02-07 09:25:07 +0000

    Fixed sku description lineitem migration for pgsql

--- src/migrations/m200207_161707_sku_description_on_lineitem.php
+++ src/migrations/m200207_161707_sku_description_on_lineitem.php
@@ -22,7 +22,7 @@ class m200207_161707_sku_description_on_lineitem extends Migration
         $query = (new Query())
             ->select(['l.id', 'l.snapshot', 'p.sku', 'p.description'])
             ->from('{{%commerce_lineitems}} l')
-            ->leftJoin('{{%commerce_purchasables}} p', 'l.purchasableId = p.id');
+            ->leftJoin('{{%commerce_purchasables}} p', '[[l.purchasableId]] = p.id');
 
         foreach ($query->batch(300) as $results) {
             foreach ($results as $row) {
