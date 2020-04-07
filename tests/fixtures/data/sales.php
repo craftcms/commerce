9c6ad983f17dadfc8ae54c4eeae7a8773eec5911
Nathaniel Hammond <nathanielhammond@gmail.com>
2020-04-07 11:38:05 +0100

    Added Sales fixture

---
+++ tests/fixtures/data/sales.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    [
+        'id' => '1000',
+        'name' => 'My Percentage Sale',
+        'description' => 'My test percentage sale.',
+        'enabled' => 1,
+        'sortOrder' => 1,
+        'dateFrom' => null,
+        'dateTo' => null,
+        'apply' => 'byPercent',
+        'applyAmount' => -0.1000,
+        'allGroups' => 1,
+        'allPurchasables' => 0,
+        'allCategories' => 1,
+        'ignorePrevious' => null,
+        'stopProcessing' => null,
+        'categoryRelationshipType' => 'sourceElement',
+    ]
+];
