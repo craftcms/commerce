8d80be30f97dac8f2ea8acfda83f819eaa6d1b79
Luke Holder <lukemh@gmail.com>
2020-04-09 00:53:20 +0800

    Reduce memory load

--- src/migrations/m200207_161707_sku_description_on_lineitem.php
+++ src/migrations/m200207_161707_sku_description_on_lineitem.php
@@ -22,29 +22,45 @@ class m200207_161707_sku_description_on_lineitem extends Migration
         if (!$this->db->columnExists('{{%commerce_lineitems}}', 'description')) {
             $this->addColumn('{{%commerce_lineitems}}', 'description', $this->string());
         }
+
         $query = (new Query())
             ->select(['[[l.id]]', '[[l.snapshot]]', '[[p.sku]]', '[[p.description]]', '[[l.purchasableId]]'])
             ->from('{{%commerce_lineitems}} l')
-            ->innerJoin('{{%commerce_purchasables}} p', '[[l.purchasableId]] = [[p.id]]');
+            ->innerJoin('{{%commerce_purchasables}} p', '[[l.purchasableId]] = [[p.id]]')
+            ->innerJoin('{{%commerce_orders}} o', '[[l.orderId]] = [[o.id]]')
+            ->where(['[[o.isCompleted]]' => true]);
 
-        $purchasableSkuAndDescriptionByPurchasableId = [];
+        $count = (new Query())
+            ->select(['[[l.id]]', '[[l.snapshot]]', '[[p.sku]]', '[[p.description]]', '[[l.purchasableId]]'])
+            ->from('{{%commerce_lineitems}} l')
+            ->innerJoin('{{%commerce_purchasables}} p', '[[l.purchasableId]] = [[p.id]]')
+            ->innerJoin('{{%commerce_orders}} o', '[[l.orderId]] = [[o.id]]')
+            ->where(['[[o.isCompleted]]' => true])
+            ->count();
+
+        echo '    > There are ' . $count . ' line items to be updated with description and sku (moved from snapshot to columns).';
+
+        $runningTotal = 0;
         foreach ($query->batch(300) as $results) {
+            $purchasableSkuAndDescriptionByPurchasableId = [];
             foreach ($results as $row) {
-                $options = Json::decodeIfJson($row['snapshot'], true);
+                $snapshot = Json::decodeIfJson($row['snapshot'], true);
                 $purchasableId = $row['purchasableId'];
-                $sku = $options['sku'] ?? $row['sku'] ?? '';
-                $description = $options['description'] ?? $row['description'] ?? '';
+                $sku = $snapshot['sku'] ?? $row['sku'] ?? '';
+                $description = $snapshot['description'] ?? $row['description'] ?? '';
                 if (!isset($purchasableSkuAndDescriptionByPurchasableId[$purchasableId])) {
                     $purchasableSkuAndDescriptionByPurchasableId[$purchasableId] = compact('description', 'sku', 'purchasableId');
                 }
             }
+            foreach ($purchasableSkuAndDescriptionByPurchasableId as $data) {
+                $this->update('{{%commerce_lineitems}}', $data, ['purchasableId' => $data['purchasableId']]);
+            }
+            $runningTotal += count($purchasableSkuAndDescriptionByPurchasableId);
+            echo '    > Updated ' . $runningTotal . ' / ' . $count . ' purchasbles on line items with description and sku.';
+            unset($purchasableSkuAndDescriptionByPurchasableId);
         }
 
-        foreach ($purchasableSkuAndDescriptionByPurchasableId as $data) {
-            $this->update('{{%commerce_lineitems}}', $data, ['purchasableId' => $data['purchasableId']]);
-        }
-
-        echo '    > Updated ' . count($purchasableSkuAndDescriptionByPurchasableId) . ' purchasbles on line items with description and sku.';
+        echo '    > Updated All line items with description and sku.';
     }
 
     /**
