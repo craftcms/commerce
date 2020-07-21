fcb19a7a4465d6bb31d4900f3c847aaf75c75f46
Nathaniel Hammond <nathanielhammond@gmail.com>
2020-07-21 15:45:58 +0100

    Payment currencies fixture

---
+++ tests/fixtures/data/payment-currencies.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'craftCoin' => [
+        'iso' => 'gbp',
+        'rate' => 0.75,
+        'primary' => false,
+    ],
+    'ptTokens' => [
+        'iso' => 'eur',
+        'rate' => 0.8,
+        'primary' => false,
+    ],
+];
