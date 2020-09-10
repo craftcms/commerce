cb758a3989bb7bb5d44b5e8cd5f37f0645164719
Nathaniel Hammond <nathanielhammond@gmail.com>
2020-09-10 15:19:59 +0100

    Fix migration checking project config schema version

--- src/migrations/m200910_134928_fix_productType_title_format_columns.php
+++ src/migrations/m200910_134928_fix_productType_title_format_columns.php
@@ -27,11 +27,14 @@ class m200910_134928_fix_productType_title_format_columns extends Migration
             $this->update('{{%commerce_producttypes}}', ['hasProductTitleField' => true], ['uid' => $productTypeUids]);
 
             $projectConfig = \Craft::$app->getProjectConfig();
-            foreach ($projectConfig->get('commerce.productTypes') ?? [] as $uid => $productType) {
-                if (in_array($uid, $productTypeUids, false)) {
-                    $productType['hasProductTitleField'] = true;
-                    $productType['productTitleFormat'] = '';
-                    $projectConfig->set("commerce.productTypes.{$uid}", $productType);
+            $schemaVersion = $projectConfig->get('plugins.commerce.schemaVersion', true);
+            if (version_compare($schemaVersion, '3.2.6', '<')) {
+                foreach ($projectConfig->get('commerce.productTypes') ?? [] as $uid => $productType) {
+                    if (in_array($uid, $productTypeUids, false)) {
+                        $productType['hasProductTitleField'] = true;
+                        $productType['productTitleFormat'] = '';
+                        $projectConfig->set("commerce.productTypes.{$uid}", $productType);
+                    }
                 }
             }
         }
