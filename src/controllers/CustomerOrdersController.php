04dcf1027498c7ad09eee569017636caf606d9d3
Nathaniel Hammond <nathanielhammond@gmail.com>
2020-09-17 09:42:21 +0100

    Added the ability to retrieve a customer’s addresses and previous orders via ajax. fixes #1682

---
+++ src/controllers/CustomerOrdersController.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @link https://craftcms.com/
+ * @copyright Copyright (c) Pixel & Tonic, Inc.
+ * @license https://craftcms.github.io/license/
+ */
+
+namespace craft\commerce\controllers;
+
+use Craft;
+use craft\commerce\Plugin;
+use yii\web\BadRequestHttpException;
+use yii\web\Response;
+
+/**
+ * Class Customer Orders Controller
+ *
+ * @author Pixel & Tonic, Inc. <support@pixelandtonic.com>
+ * @since 3.x
+ */
+class CustomerOrdersController extends BaseFrontEndController
+{
+    /**
+     * Get customer's orders
+     *
+     * @return Response
+     * @throws BadRequestHttpException
+     */
+    public function actionGetOrders(): Response
+    {
+        $this->requireAcceptsJson();
+
+        $customer = Plugin::getInstance()->getCustomers()->getCustomer();
+        $orders = $customer->getOrders();
+
+        return $this->asJson(['success' => true, 'orders' => $orders]);
+    }
+}
