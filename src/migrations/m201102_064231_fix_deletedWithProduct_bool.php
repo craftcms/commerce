feca906e9ecafa1c9a8d86cf401e19b672392636
Luke Holder <lukemh@gmail.com>
2020-11-02 20:41:36 +0800

    No needed

--- src/migrations/m201102_064231_fix_deletedWithProduct_bool.php
+++ src/migrations/m201102_064231_fix_deletedWithProduct_bool.php
@@ -21,34 +21,17 @@ class m201102_064231_fix_deletedWithProduct_bool extends Migration
      */
     public function safeUp()
     {
-        $variants = (new Query())
-            ->select(['id', '[[deletedWithProduct]]'])
-            ->where('[[deletedWithProduct]] != NULL')
-            ->where(['not', ['[[deletedWithProduct]]' => null]])
-            ->from(['{{%commerce_variants}}'])
-            ->all($this->db);
-
         // Now we can set the track column to NOT NULL
         if ($this->db->getIsPgsql()) {
             // Manually construct the SQL for Postgres
             // (see https://github.com/yiisoft/yii2/issues/12077)
 
             $this->execute('ALTER TABLE {{%commerce_variants}} ALTER COLUMN [[deletedWithProduct]] DROP DEFAULT;');
-            $this->execute('ALTER TABLE {{%commerce_variants}} ALTER [[deletedWithProduct]] TYPE bool USING CASE WHEN [[deletedWithProduct]]=1 THEN TRUE ELSE FALSE END;');
+            $this->execute('ALTER TABLE {{%commerce_variants}} ALTER [[deletedWithProduct]] TYPE bool USING CASE WHEN [[deletedWithProduct]]=1 THEN TRUE CASE WHEN [[deletedWithProduct]]=0 THEN FALSE ELSE NULL END;');
             $this->execute('ALTER TABLE {{%commerce_variants}} ALTER COLUMN [[deletedWithProduct]] SET DEFAULT NULL;');
 
         } else {
             $this->alterColumn('{{%commerce_variants}}', '[[deletedWithProduct]]', $this->boolean()->null());
-        }
-
-        Craft::$app->getDb()->createCommand()->update('{{%commerce_variants}}', ['[[deletedWithProduct]]' => null])->execute();
-
-        foreach ($variants as $variant) {
-            Craft::$app->getDb()->createCommand()->update(
-                '{{%commerce_variants}}',
-                ['[[deletedWithProduct]]' => (bool)$variant['deletedWithProduct']],
-                ['id' => $variant['id']])
-                ->execute();
         }
     }
 
