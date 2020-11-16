bc0ddeb7a7fe9e75321ca44465163292e1fabf6f
Dale Ramirez <dalefirstpage@gmail.com>
2020-11-16 16:54:42 +0800

    Added remove address method comment description.

--- src/elements/Order.php
+++ src/elements/Order.php
@@ -2366,13 +2366,19 @@ class Order extends Element
             }
         }
     }
-
+    
+    /**
+     * Removes the shipping address from the order
+     */
     public function removeShippingAddress()
     {
         $this->shippingAddressId = null;
         $this->_shippingAddress = null;
     }
-
+    
+    /**
+     * Removes the billing address from the order
+     */
     public function removeBillingAddress()
     {
         $this->billingAddressId = null;
