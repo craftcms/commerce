4339e2b362a769503e8ef50968983f05d93643e0
Dale Ramirez <dalefirstpage@gmail.com>
2020-12-14 22:30:59 +0800

    Fixed wrong column exist check

--- src/migrations/m201207_065953_add_dataDeleted_column_archive_and_tax_categories.php
+++ src/migrations/m201207_065953_add_dataDeleted_column_archive_and_tax_categories.php
@@ -15,11 +15,11 @@ class m201207_065953_add_dataDeleted_column_archive_and_tax_categories extends M
      */
     public function safeUp()
     {
-        if (!$this->db->columnExists('{{%commerce_shippingcategories}}', 'pdfLanguage')) {
+        if (!$this->db->columnExists('{{%commerce_shippingcategories}}', 'dateDeleted')) {
             $this->addColumn('{{%commerce_shippingcategories}}', 'dateDeleted', $this->dateTime()->after('dateUpdated'));
         }        
         
-        if (!$this->db->columnExists('{{%commerce_taxcategories}}', 'pdfLanguage')) {
+        if (!$this->db->columnExists('{{%commerce_taxcategories}}', 'dateDeleted')) {
             $this->addColumn('{{%commerce_taxcategories}}', 'dateDeleted', $this->dateTime()->after('dateUpdated'));
         }
     }
