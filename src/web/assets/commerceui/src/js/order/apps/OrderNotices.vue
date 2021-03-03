55b3162ca3733e6ba170e789d6226136f5d99539
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2021-03-03 16:05:22 +0000

    Update order notices design

--- src/web/assets/commerceui/src/js/order/apps/OrderNotices.vue
+++ src/web/assets/commerceui/src/js/order/apps/OrderNotices.vue
@@ -1,61 +1,74 @@
 <template>
-    <div v-if="draft && draft.order && draft.order.notices && Object.keys(draft.order.notices).length">
-        <div class="meta read-only">
-            <button @click.prevent="clearNotices" class="btn small right">clear</button>
-            <ul>
-                <li v-for="(notice, key) in draft.order.notices" :key="key">
+    <div v-if="showNotices">
+        <div class="meta read-only warning">
+            <div class="order-flex order-notices-header">
+                <div>
+                    {{ 'Customer Notices'|t('commerce') }}
+                </div>
+                <div>
+                    <button @click.prevent="clearNotices" class="btn small">{{ 'Clear notices'|t('commerce') }}</button>
+                </div>
+            </div>
+            <div class="order-notices-items">
+                <div v-for="(notice, key) in draft.order.notices" :key="key">
                     <div v-for="(item, index) in notice" :key="index">
-                        {{ item }}
+                        <hr>
+                        <div class="order-notices-item">
+                            {{ item }}
+                        </div>
                     </div>
-                </li>
-            </ul>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
-import {mapActions, mapGetters, mapState} from 'vuex'
-import mixins from '../mixins'
 
-export default {
-    name: 'order-notices-app',
-    // data(){
-    //   return {
-    //       draft: {
-    //           order: {
-    //               notices : {
-    //                   'couponCode': ['Removed'],
-    //                   'lineItems': ['price changed'],
-    //               }
-    //           }
-    //       }
-    //   }
-    // },
-    components: {},
+    export default {
+        name: 'order-notices-app',
 
-    mixins: [mixins],
+        computed: {
 
-    computed: {
-        ...mapState({
-            draft: state => state.draft,
-        }),
+            draft: {
+                get() {
+                    return JSON.parse(JSON.stringify(this.$store.state.draft))
+                },
 
-        ...mapGetters([]),
-    },
+                set(draft) {
+                    this.$store.commit('updateDraft', draft)
+                }
+            },
 
-    methods: {
-        ...mapActions([]),
-        clearNotices() {
-            let draft = this.draft;
-            draft.order.notices = {};
-            this.$store.commit('updateDraft', draft);
+            showNotices() {
+                return this.draft && this.draft.order && this.draft.order.notices && Object.keys(this.draft.order.notices).length
+            }
+        },
+
+        methods: {
+            clearNotices() {
+                let draft = this.draft;
+                draft.order.notices = {};
+                this.draft = draft;
+            }
         }
     }
-}
 </script>
 
 <style lang="scss">
-@import "../../../../node_modules/craftcms-sass/src/mixins";
+    @import "../../../../node_modules/craftcms-sass/src/mixins";
 
+    .order-notices-header {
+        align-items: center;
+        justify-content: space-between;
+        padding-bottom: 14px;
+    }
 
+    .order-notices-items {
+        margin-bottom: -14px;
+    }
+
+    .order-notices-item {
+        padding-bottom: 14px;
+    }
 </style>
