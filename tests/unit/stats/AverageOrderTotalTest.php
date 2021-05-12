5f33bf86fb8b8027ee737b4bf583a6ee4507c8c0
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2021-05-12 10:04:07 +0100

    Added AverageOrderTotalTest

---
+++ tests/unit/stats/AverageOrderTotalTest.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * @link https://craftcms.com/
+ * @copyright Copyright (c) Pixel & Tonic, Inc.
+ * @license https://craftcms.github.io/license/
+ */
+
+namespace craftcommercetests\unit\stats;
+
+use Codeception\Test\Unit;
+use craft\commerce\stats\AverageOrderTotal;
+use craftcommercetests\fixtures\OrdersFixture;
+use DateTime;
+use UnitTester;
+
+/**
+ * AverageOrderTotalTest
+ *
+ * @author Pixel & Tonic, Inc. <support@pixelandtonic.com>
+ * @since 3.x
+ */
+class AverageOrderTotalTest extends Unit
+{
+    /**
+     * @var UnitTester
+     */
+    protected $tester;
+
+    /**
+     * @return array
+     */
+    public function _fixtures(): array
+    {
+        return [
+            'orders' => [
+                'class' => OrdersFixture::class,
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider getDataDataProvider
+     *
+     * @param string $dateRange
+     * @param DateTime $startDate
+     * @param DateTime $endDate
+     * @param float|null $average
+     */
+    public function testGetData(string $dateRange, DateTime $startDate, DateTime $endDate, $average): void
+    {
+        $stat = new AverageOrderTotal($dateRange, $startDate, $endDate);
+        $data = $stat->get();
+
+        if ($average === null) {
+            $this->tester->assertEquals($average, $data);
+        } else {
+            $this->tester->assertIsNumeric($data);
+        }
+        $this->tester->assertEquals($average, $data);
+    }
+
+    /**
+     * @return array[]
+     */
+    public function getDataDataProvider(): array
+    {
+        return [
+            [
+                AverageOrderTotal::DATE_RANGE_TODAY,
+                (new DateTime('now', new \DateTimeZone('America/Los_Angeles')))->setTime(0, 0),
+                (new DateTime('now', new \DateTimeZone('America/Los_Angeles')))->setTime(0, 0),
+                41.98,
+            ],
+            [
+                AverageOrderTotal::DATE_RANGE_CUSTOM,
+                (new DateTime('7 days ago', new \DateTimeZone('America/Los_Angeles')))->setTime(0, 0),
+                (new DateTime('5 days ago', new \DateTimeZone('America/Los_Angeles')))->setTime(0, 0),
+                null,
+            ],
+        ];
+    }
+}
