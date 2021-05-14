129e7ae55d47c23ee2bcd7803f6166014cb51ef3
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2021-05-14 13:01:46 +0100

    Added TopProductTypesTest

---
+++ tests/unit/stats/TopProductTypesTest.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * @link https://craftcms.com/
+ * @copyright Copyright (c) Pixel & Tonic, Inc.
+ * @license https://craftcms.github.io/license/
+ */
+
+namespace craftcommercetests\unit\stats;
+
+use Codeception\Test\Unit;
+use craft\commerce\models\ProductType;
+use craft\commerce\stats\TopProducts;
+use craft\commerce\stats\TopProductTypes;
+use craftcommercetests\fixtures\OrdersFixture;
+use DateTime;
+use UnitTester;
+
+/**
+ * TopProductTypesTest
+ *
+ * @author Pixel & Tonic, Inc. <support@pixelandtonic.com>
+ * @since 3.x
+ */
+class TopProductTypesTest extends Unit
+{
+    /**
+     * @var UnitTester
+     */
+    protected $tester;
+
+    /**
+     * @return array
+     */
+    public function _fixtures(): array
+    {
+        return [
+            'orders' => [
+                'class' => OrdersFixture::class,
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider getDataDataProvider
+     *
+     * @param string $dateRange
+     * @param string $type
+     * @param DateTime $startDate
+     * @param DateTime $endDate
+     * @param int $count
+     * @param array $productTypeData
+     */
+    public function testGetData(string $dateRange,  string $type, DateTime $startDate, DateTime $endDate, int $count, array $productTypeData): void
+    {
+        $stat = new TopProductTypes($dateRange, $type, $startDate, $endDate);
+        $data = $stat->get();
+
+        $this->tester->assertIsArray($data);
+        $this->tester->assertCount($count, $data);
+
+        if ($count !== 0) {
+            $topProduct = array_shift($data);
+
+            $testKeys = ['id', 'name', 'qty', 'revenue', 'productType'];
+            foreach ($testKeys as $testKey) {
+                $this->tester->assertArrayHasKey($testKey, $topProduct);
+
+                if ($testKey === 'productType') {
+                    $this->tester->assertInstanceOf(ProductType::class, $topProduct[$testKey]);
+                } else {
+                    $this->tester->assertEquals($productTypeData[$testKey], $topProduct[$testKey]);
+                }
+            }
+        }
+    }
+
+    /**
+     * @return array[]
+     */
+    public function getDataDataProvider(): array
+    {
+        return [
+            [
+                TopProducts::DATE_RANGE_TODAY,
+                'revenue',
+                (new DateTime('now', new \DateTimeZone('America/Los_Angeles')))->setTime(0, 0),
+                (new DateTime('now', new \DateTimeZone('America/Los_Angeles')))->setTime(0, 0),
+                1,
+                [
+                    'id' => 2001,
+                    'name' => 'T-Shirts',
+                    'qty' => 4,
+                    'revenue' => 83.96,
+                ]
+            ],
+            [
+                TopProducts::DATE_RANGE_CUSTOM,
+                'revenue',
+                (new DateTime('7 days ago', new \DateTimeZone('America/Los_Angeles')))->setTime(0, 0),
+                (new DateTime('5 days ago', new \DateTimeZone('America/Los_Angeles')))->setTime(0, 0),
+                0,
+                []
+            ],
+        ];
+    }
+}
