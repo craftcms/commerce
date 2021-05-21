aa745aeb05a512ac4da00106666cda0e365abcf1
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2021-05-21 09:57:56 +0100

    Revert all test debug code

--- tests/unit/stats/TotalOrdersTest.php
+++ tests/unit/stats/TotalOrdersTest.php
@@ -60,31 +60,6 @@ class TotalOrdersTest extends Unit
         self::assertIsArray($data['chart']);
         self::assertArrayHasKey($startDate->format('Y-m-d'), $data['chart']);
         self::assertArrayHasKey($endDate->format('Y-m-d'), $data['chart']);
-
-        codecept_debug('date_default_timezone_get: '.date_default_timezone_get());
-        codecept_debug("data['total']: ".$data['total']);
-        codecept_debug('$daysDiff + 1: ' . ($daysDiff + 1));
-        codecept_debug("count(data['chart']): ".count($data['chart']));
-        foreach ($data['chart'] as $outerArrayKey => $outerArrayValue) {
-            codecept_debug('$outerArrayKey: '.($outerArrayKey === '' ? '\'\'' : $outerArrayKey));
-            foreach ($outerArrayValue as $arrayKey => $arrayValue) {
-                codecept_debug('$arrayKey => $arrayValue: '.$arrayKey.' => '.(is_null($arrayValue) ? 'null' : $arrayValue));
-            }
-        }
-
-        $results = \Craft::$app->getDb()->createCommand(
-        'SELECT * FROM `commerce_orders` `orders` INNER JOIN `elements` `elements` ON `elements`.`id` = `orders`.`id` WHERE (`dateOrdered` >= \'2021-05-20 07:00:00\') AND (`dateOrdered` <= \'2021-05-21 06:59:59\') AND (`isCompleted`=1) AND (`elements`.`dateDeleted` IS NULL)'
-        )->queryAll();
-
-        codecept_debug('count($results): '.count($results));
-
-        foreach ($results as $num => $result) {
-            codecept_debug('row '.$num.': ');
-            foreach ($result as $column => $value) {
-                codecept_debug($column.': '.$value);
-            }
-        }
-
         self::assertCount($daysDiff + 1, $data['chart']);
 
         $firstItem = array_shift($data['chart']);
