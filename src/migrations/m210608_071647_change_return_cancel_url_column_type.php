8697ea5ba08ada8fd7fe3e3fc5d57d1700666c01
Dale Ramirez <dalefirstpage@gmail.com>
2021-06-10 18:45:10 +0800

    Fixed pgsql column alter migration

--- src/migrations/m210608_071647_change_return_cancel_url_column_type.php
+++ src/migrations/m210608_071647_change_return_cancel_url_column_type.php
@@ -18,8 +18,8 @@ class m210608_071647_change_return_cancel_url_column_type extends Migration
         if ($this->db->getIsPgsql()) {
             // Manually construct the SQL for Postgres
             // (see https://github.com/yiisoft/yii2/issues/12077)
-            $this->execute('alter table {{%commerce_orders} alter column [[returnUrl]] DROP NOT NULL');
-            $this->execute('alter table {{%commerce_orders}} alter column [[cancelUrl]] DROP NOT NULL');
+            $this->execute('alter table {{%commerce_orders}} alter column [[returnUrl]] TYPE TEXT');
+            $this->execute('alter table {{%commerce_orders}} alter column [[cancelUrl]] TYPE TEXT');
         } else {
             $this->alterColumn('{{%commerce_orders}}', 'returnUrl', $this->text());
             $this->alterColumn('{{%commerce_orders}}', 'cancelUrl', $this->text());
