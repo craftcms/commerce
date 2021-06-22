1c454582fedca22954cea82e9bdacf28117d2712
Dale Ramirez <dalefirstpage@gmail.com>
2021-06-22 16:24:28 +0800

    Moved product detailed permission migration to detailed permission migration class

--- src/migrations/m210614_073359_detailed_permission.php
+++ src/migrations/m210614_073359_detailed_permission.php
@@ -2,9 +2,11 @@
 
 namespace craft\commerce\migrations;
 
+use Craft;
 use craft\db\Migration;
 use craft\db\Query;
 use craft\db\Table;
+use yii\db\Expression;
 
 /**
  * m210614_073359_detailed_permission migration.
@@ -16,8 +18,10 @@ class m210614_073359_detailed_permission extends Migration
      */
     public function safeUp()
     {
+        $this->_detailedProducts();
         $this->_detailedPromotions();
         $this->_detailedSubscriptions();
+        $this->_projectConfigUpdates();
     }
 
     /**
@@ -28,7 +32,7 @@ class m210614_073359_detailed_permission extends Migration
         echo "m210614_073359_detailed_permission cannot be reverted.\n";
         return false;
     }
-    
+
     private function _detailedPromotions()
     {
         // Create new promotion permissions
@@ -91,11 +95,11 @@ class m210614_073359_detailed_permission extends Migration
     private function _detailedSubscriptions()
     {
         $this->insert(Table::USERPERMISSIONS, ['name' => 'commerce-editsubscriptions']);
-        $editSubscriptionId = $this->db->getLastInsertID();        
-        
+        $editSubscriptionId = $this->db->getLastInsertID();
+
         $this->insert(Table::USERPERMISSIONS, ['name' => 'commerce-createsubscriptions']);
-        $createSubscriptionId = $this->db->getLastInsertID();        
-        
+        $createSubscriptionId = $this->db->getLastInsertID();
+
         $this->insert(Table::USERPERMISSIONS, ['name' => 'commerce-deletesubscriptions']);
         $deleteSubscriptionId = $this->db->getLastInsertID();
 
@@ -129,5 +133,95 @@ class m210614_073359_detailed_permission extends Migration
             $this->insert(Table::USERPERMISSIONS_USERGROUPS, ['groupId' => $groupSubscription['groupId'], 'permissionId' => $deleteSubscriptionId]);
         }
 
+    }
+
+    private function _projectConfigUpdates()
+    {
+        // Make project config updates
+        $projectConfig = Craft::$app->getProjectConfig();
+        $schemaVersion = $projectConfig->get('plugins.commerce.schemaVersion', true);
+
+        if (version_compare($schemaVersion, '4.0', '<')) {
+            $groups = (new Query())
+                ->select(['id', 'name', 'uid'])
+                ->from(['groups' => Table::USERGROUPS])
+                ->all();
+
+            $setGroupPermissions = [];
+
+            foreach ($groups as $group) {
+                $groupPermissions = (new Query())
+                    ->select(['up.name'])
+                    ->from(['up_ug' => Table::USERPERMISSIONS_USERGROUPS])
+                    ->where(['up_ug.groupId' => $group['id']])
+                    ->innerJoin(['up' => Table::USERPERMISSIONS], '[[up.id]] = [[up_ug.permissionId]]')
+                    ->column();
+
+                $setGroupPermissions[$group['uid']] = $groupPermissions;
+            }
+
+            foreach ($setGroupPermissions as $uid => $setGroupPermission) {
+                $projectConfig->set('users.groups.' . $uid . '.permissions', $setGroupPermission);
+            }
+        }
+    }
+
+    private function _detailedProducts()
+    {
+        // Get existing manage product type permission
+        $permissions = (new Query())
+            ->select(['id', 'name'])
+            ->from([Table::USERPERMISSIONS])
+            ->where(new Expression("LEFT([[name]], 26) = 'commerce-manageproducttype'"))
+            ->all();
+
+        if (count($permissions) > 0) {
+
+            foreach ($permissions as $permission) {
+
+                $permissionName = explode(':', $permission['name']);
+                $productTypeUid = $permissionName[1];
+
+                // Rename manage product type to edit product type
+                $newName = str_replace('commerce-manageproducttype', 'commerce-editproducttype', $permission['name']);
+                $this->update(Table::USERPERMISSIONS, ['name' => $newName], ['id' => $permission['id']], [], false);
+
+                // Create new create product permission by product type
+                $this->insert(Table::USERPERMISSIONS, ['name' => 'commerce-createproducts:' . $productTypeUid]);
+                $createPermissionId = $this->db->getLastInsertID();
+
+                // Create new delete product permission by product type
+                $this->insert(Table::USERPERMISSIONS, ['name' => 'commerce-deleteproducts:' . $productTypeUid]);
+                $deletePermissionId = $this->db->getLastInsertID();
+
+                // Check if manage product type is ticked for user permissions
+                $manageProductTypes = (new Query())
+                    ->select(['id', 'permissionId', 'userId'])
+                    ->from([Table::USERPERMISSIONS_USERS])
+                    ->where(['permissionId' => $permission['id']])
+                    ->all();
+                // Add the new edit product child permissions for the same users
+                foreach ($manageProductTypes as $manageProductType) {
+                    $this->insert(Table::USERPERMISSIONS_USERS, ['userId' => $manageProductType['userId'], 'permissionId' => $createPermissionId]);
+                    $this->insert(Table::USERPERMISSIONS_USERS, ['userId' => $manageProductType['userId'], 'permissionId' => $deletePermissionId]);
+                }
+
+                // Check if manage product type is ticked for user group permissions
+                $manageProductTypesForGroups = (new Query())
+                    ->select(['id', 'permissionId', 'groupId'])
+                    ->from([Table::USERPERMISSIONS_USERGROUPS])
+                    ->where(['permissionId' => $permission['id']])
+                    ->all();
+                // Add the new edit product child permissions for the same groups
+                foreach ($manageProductTypesForGroups as $manageProductType) {
+                    // Create new create and delete product permission relationship with a group.
+                    $this->insert(Table::USERPERMISSIONS_USERGROUPS, ['groupId' => $manageProductType['groupId'], 'permissionId' => $createPermissionId]);
+                    $this->insert(Table::USERPERMISSIONS_USERGROUPS, ['groupId' => $manageProductType['groupId'], 'permissionId' => $deletePermissionId]);
+                }
+            }
+
+            // No longer need this top level permission
+            $this->delete(Table::USERPERMISSIONS, ['name' => 'commerce-manageproducts']);
+        }
     }
 }
