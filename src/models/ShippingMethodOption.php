f307ccfa78b9bafce8421ee52c9b99159d483a74
Luke Holder <luke@pixelandtonic.com>
2021-06-30 16:38:09 +0800

    modify availableShippingMethodOptions
    - Shipping method options should return all when the order is completed.
    - only change the shipping method during recalculation when ‘recalculate all’ is used
    - remove the dynamic shipping method on the order inside the getShippingMethod

--- src/models/ShippingMethodOption.php
+++ src/models/ShippingMethodOption.php
@@ -35,6 +35,11 @@ class ShippingMethodOption extends ShippingMethod
     public $price;
 
     /**
+     * @var
+     */
+    public $matchesOrder;
+
+    /**
      * @return array
      */
     public function behaviors(): array
