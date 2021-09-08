41ce6dc972c1445d375aaa9e5d1974f9c9c443d3
Dale <dalefirstpage@gmail.com>
2021-09-08 20:10:48 +0800

    - Added install migration for the new address attributes - Created migration class for renaming address columns

---
+++ src/migrations/m210908_092553_address_international_support_updates.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace craft\commerce\migrations;
+
+use Craft;
+use craft\db\Migration;
+
+/**
+ * m210908_092553_address_international_support_updates migration.
+ */
+class m210908_092553_address_international_support_updates extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp(): bool
+    {
+        $this->renameColumn('{{%commerce_addresses}}', 'stateId', 'administrativeAreaId');
+        $this->renameColumn('{{%commerce_addresses}}', 'firstName', 'givenName');
+        $this->renameColumn('{{%commerce_addresses}}', 'lastName', 'familyName');
+        $this->renameColumn('{{%commerce_addresses}}', 'address1', 'addressLine1');
+        $this->renameColumn('{{%commerce_addresses}}', 'address2', 'addressLine2');
+        $this->renameColumn('{{%commerce_addresses}}', 'address3', 'addressLine3');
+        $this->renameColumn('{{%commerce_addresses}}', 'city', 'locality');
+        $this->renameColumn('{{%commerce_addresses}}', 'zipCode', 'postalCode');
+        $this->renameColumn('{{%commerce_addresses}}', 'stateName', 'administrativeAreaName');
+
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown(): bool
+    {
+        echo "m210908_092553_address_international_support_updates cannot be reverted.\n";
+        return false;
+    }
+}
