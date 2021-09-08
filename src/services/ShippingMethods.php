0230c8e6a4617560eb680cbbc3875f524abe5a52
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2021-09-08 07:49:37 +0100

    No need to typecast attributes

--- src/services/ShippingMethods.php
+++ src/services/ShippingMethods.php
@@ -75,7 +75,7 @@ class ShippingMethods extends Component
 
         foreach ($results as $result) {
             $shippingMethod = new ShippingMethod($result);
-            $shippingMethod->typecastAttributes();
+
             $this->_allShippingMethods[] = $shippingMethod;
         }
 
