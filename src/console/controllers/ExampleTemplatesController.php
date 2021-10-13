558e62a433137c0850ec8b5561bb1ab766ffbe1d
Matt Stein <matt@pixelandtonic.com>
2021-10-13 10:40:44 -0700

    Add selection box style.
    To differentiate between user input and read-only information panels.

--- src/console/controllers/ExampleTemplatesController.php
+++ src/console/controllers/ExampleTemplatesController.php
@@ -261,6 +261,7 @@ class ExampleTemplatesController extends Controller
             '[[classes.a]]' => "text-$mainColor-500 hover:text-$mainColor-600",
             '[[classes.input]]' => "border border-gray-300 hover:border-gray-500 px-4 py-2 leading-tight rounded",
             '[[classes.box.base]]' => "bg-gray-100 border-$mainColor-300 border-b-2 p-6",
+            '[[classes.box.selection]]' => "border-$mainColor-300 border-b-2 px-6 py-4 rounded-md shadow-md hover:shadow-lg",
             '[[classes.box.error]]' => "bg-$dangerColor-100 border-$dangerColor-500 border-b-2 p-6",
             '[[classes.btn.base]]' => "cursor-pointer rounded px-4 py-2 inline-block",
             '[[classes.btn.small]]' => "cursor-pointer rounded px-2 py-1 text-sm inline-block",
