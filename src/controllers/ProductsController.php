d6be3668449854cedb629cbb8388310d85ecddde
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2021-10-28 15:06:51 +0100

    Add product debug to product edit

--- src/controllers/ProductsController.php
+++ src/controllers/ProductsController.php
@@ -10,6 +10,7 @@ namespace craft\commerce\controllers;
 use Craft;
 use craft\base\Element;
 use craft\commerce\elements\Product;
+use craft\commerce\helpers\DebugPanel;
 use craft\commerce\helpers\Product as ProductHelper;
 use craft\commerce\models\ProductType;
 use craft\commerce\Plugin;
@@ -386,6 +387,9 @@ class ProductsController extends BaseController
         $productType = $variables['productType'];
         /** @var Product $product */
         $product = $variables['product'];
+
+        DebugPanel::prependModelTab('Product Type', $productType);
+        DebugPanel::prependModelTab('Product', $product);
 
         $form = $productType->getProductFieldLayout()->createForm($product);
         $variables['tabs'] = $form->getTabMenu();
