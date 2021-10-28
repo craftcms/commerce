51a60068d3dc76fe1b1e9bffd57b5eafb1c510b8
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2021-10-28 15:06:38 +0100

    Fix model view template

--- src/views/debug/commerce/model.php
+++ src/views/debug/commerce/model.php
@@ -1,24 +1,3 @@
-<?php function _panelOutputRow($attr, $value, $label = null): string {
-    $label = $label ?: $attr;
-
-    if (is_string($value)) {
-        if (strpos($attr, 'html') !== -1) {
-            $output = craft\helpers\Html::encode($value);
-        } else {
-            $output = $value;
-        }
-    } else {
-        $output = \yii\helpers\VarDumper::dumpAsString($value);
-    }
-
-    return <<<EOD
-<tr>
-    <th>$label</th>
-    <td><code>$output</code></td>
-</tr>
-EOD;
-} ?>
-
 <?php if (isset($model, $attributes, $toArrayAttributes)): ?>
     <h2 style="font-size: 0.75rem;"><?= get_class($model) ?></h2>
     <div class="table-responsive">
@@ -30,11 +9,11 @@ EOD;
                 <?php if (is_array($attrValue)): ?>
                     <?php foreach($attrValue as $key => $value): ?>
                         <?php $value = in_array($attr, $toArrayAttributes, true) ? $value->toArray() : $value; ?>
-                        <?php echo _panelOutputRow($attr, $value, sprintf('%s.%s', $attr, $key)); ?>
+                        <?php echo \craft\commerce\helpers\DebugPanel::renderModelAttributeRow($attr, $value, sprintf('%s.%s', $attr, $key)); ?>
                     <?php endforeach; ?>
                 <?php else: ?>
                     <?php $attrValue = in_array($attr, $toArrayAttributes, true) ? $attrValue->toArray() : $attrValue; ?>
-                    <?php echo _panelOutputRow($attr, $attrValue); ?>
+                    <?php echo \craft\commerce\helpers\DebugPanel::renderModelAttributeRow($attr, $attrValue); ?>
                 <?php endif; ?>
             <?php endforeach; ?>
             </tbody>
