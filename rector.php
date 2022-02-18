c1f799b601f7470916d1e80e583ca149e79f09c4
Luke Holder <luke@pixelandtonic.com>
2022-02-18 11:32:21 +0800

    PHP 8

--- rector.php
+++ rector.php
@@ -4,12 +4,7 @@ use Rector\Php74\Rector\Property\TypedPropertyRector;
 use Rector\Set\ValueObject\SetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-// here we can define, what sets of rules will be applied
-// tip: use "SetList" class to autocomplete sets
-$containerConfigurator->import(SetList::CODE_QUALITY);
-
-// register single rule
-$services = $containerConfigurator->services();
-$services->set(TypedPropertyRector::class);
+return static function(ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(SetList::CODE_QUALITY);
+    $containerConfigurator->import(SetList::PHP_80);
 };
