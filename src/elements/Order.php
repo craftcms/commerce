e8cba179a8537e57352619c69dacf44c35703cdf
Luke Holder <luke@pixelandtonic.com>
2022-03-02 22:36:33 +0800

    Move define fields into behavior

--- src/elements/Order.php
+++ src/elements/Order.php
@@ -1343,12 +1343,7 @@ class Order extends Element
                 return $model->$attribute;
             };
         }
-
-        //TODO Remove this when we require Craft 3.5 and the bahaviour can support the define fields event  #COM-27
-        if ($this->getBehavior('currencyAttributes')) {
-            $fields = array_merge($fields, $this->getBehavior('currencyAttributes')->currencyFields());
-        }
-
+        
         $fields['paidStatusHtml'] = 'paidStatusHtml';
         $fields['customerLinkHtml'] = 'customerLinkHtml';
         $fields['orderStatusHtml'] = 'orderStatusHtml';
