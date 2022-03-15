23e14cdeaeb83d19e3be3f039ac45dc46e5056f5
Luke Holder <luke@pixelandtonic.com>
2022-03-15 14:46:10 +0800

    Phpdocs

--- src/services/Customers.php
+++ src/services/Customers.php
@@ -137,6 +137,8 @@ class Customers extends Component
     }
 
     /**
+     * Returns a customer record by a user element, creating one if none already exists.
+     * 
      * @param User $user
      * @return CustomerRecord
      */
