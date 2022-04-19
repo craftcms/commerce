13ccf8dc0042528aafc912ce0d71f1ba327b7f60
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2022-04-19 09:26:02 +0100

    Revert "Account for the use of the `COMPOSER_VENDOR_DIR` env variable in tests"
    This reverts commit 2e3e28cb443ba72b0abfd608fcc8930f42437820.

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -17,7 +17,7 @@ define('CRAFT_TEMPLATES_PATH', __DIR__ . DIRECTORY_SEPARATOR . '_craft' . DIRECT
 define('CRAFT_CONFIG_PATH', __DIR__ . DIRECTORY_SEPARATOR . '_craft' . DIRECTORY_SEPARATOR . 'config');
 define('CRAFT_MIGRATIONS_PATH', __DIR__ . DIRECTORY_SEPARATOR . '_craft' . DIRECTORY_SEPARATOR . 'migrations');
 define('CRAFT_TRANSLATIONS_PATH', __DIR__ . DIRECTORY_SEPARATOR . '_craft' . DIRECTORY_SEPARATOR . 'translations');
-define('CRAFT_VENDOR_PATH', getenv('COMPOSER_VENDOR_DIR') ?? dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor');
+define('CRAFT_VENDOR_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor');
 
 $devMode = true;
 
