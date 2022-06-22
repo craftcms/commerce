0e0688a63658992321d65a9407e01f12d1523f3b
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2022-06-22 13:27:50 +0100

    Added shipping zones fixture

---
+++ tests/fixtures/data/shipping-zones.php
@@ -0,0 +1,41 @@
+<?php
+
+use craft\commerce\elements\conditions\addresses\ZoneAddressCondition;
+use craft\elements\conditions\addresses\CountryConditionRule;
+
+return [
+    'us-zone' => [
+        'name' => 'US Zone',
+        'description' => 'US Zone',
+        'condition' => [
+            'elementType' => null,
+            'fieldContext' => 'global',
+            'class' => ZoneAddressCondition::class,
+            'conditionRules' => [
+                [
+                    'class' => CountryConditionRule::class,
+                    'uid' => '03bfbb2a-341d-4d2b-905c-c56ec130546f',
+                    'operator' => 'in',
+                    'values' => ['US']
+                ],
+            ],
+        ],
+    ],
+    'eu-zone' => [
+        'name' => 'EU Zone',
+        'description' => 'EU Zone',
+        'condition' => [
+            'elementType' => null,
+            'fieldContext' => 'global',
+            'class' => ZoneAddressCondition::class,
+            'conditionRules' => [
+                [
+                    'class' => CountryConditionRule::class,
+                    'uid' => '03bfbb2a-341d-4d2b-905c-c56ec130546Z',
+                    'operator' => 'in',
+                    'values' => ['GB', 'FR']
+                ],
+            ],
+        ],
+    ],
+];
