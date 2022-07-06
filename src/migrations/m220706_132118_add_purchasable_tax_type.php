3d027b2cd9e304237980eb61745d5be2de953085
Luke Holder <luke@pixelandtonic.com>
2022-07-06 21:45:53 +0800

    WIP New taxable subject purchasable
    49.17 * 1.2 = 59.004 (20% VAT should be rounded on a unit level)
    59 * 4 = 236
    
    previous would end up with 236.02 tax of 20% using line item item total (49.17*4 * 1.2)

---
+++ src/migrations/m220706_132118_add_purchasable_tax_type.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace craft\commerce\migrations;
+
+use Craft;
+use craft\db\Migration;
+
+/**
+ * m220706_132118_add_purchasable_tax_type migration.
+ */
+class m220706_132118_add_purchasable_tax_type extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp(): bool
+    {
+        $values = ['purchasable', 'price', 'shipping', 'price_shipping', 'order_total_shipping', 'order_total_price'];
+        if ($this->db->getIsPgsql()) {
+            // Manually construct the SQL for Postgres
+            $check = '[[taxable]] in (';
+            foreach ($values as $i => $value) {
+                if ($i != 0) {
+                    $check .= ',';
+                }
+                $check .= $this->db->quoteValue($value);
+            }
+            $check .= ')';
+            $this->execute("alter table {{%commerce_taxrates}} drop constraint {{%commerce_taxrates_taxable_check}}, add check ({$check})");
+        } else {
+            $this->alterColumn('{{%commerce_taxrates}}', 'taxable', $this->enum('taxable', $values));
+        }
+
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown(): bool
+    {
+        echo "m220706_132118_add_purchasable_tax_type cannot be reverted.\n";
+        return false;
+    }
+}
