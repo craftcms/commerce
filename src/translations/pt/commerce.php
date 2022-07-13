b6971abee10ce708ce194940e9aad2d7feecbf1b
shinybrad <83238715+shinybrad@users.noreply.github.com>
2022-07-13 11:11:24 -0400

    New translations commerce.php (Portuguese)

--- src/translations/pt/commerce.php
+++ src/translations/pt/commerce.php
@@ -965,7 +965,7 @@ return [
     'That handle is already in use' => 'Essa pega já está a ser usada',
     'That handle is already in use.' => 'Esta pega já está a ser usada.',
     'The PDF to attach to this email.' => 'O PDF a anexar a este email.',
-    'The URL to the page that will hold the page for updating billing details for a subscription as well as handle 3DS authentication.' => 'The URL to the page that will hold the page for updating billing details for a subscription as well as handle 3DS authentication.',
+    'The URL to the page for updating billing details for a subscription, as well as handling 3DS authentication.' => 'The URL to the page for updating billing details for a subscription, as well as handling 3DS authentication.',
     'The address provided is outside the store’s market.' => 'O endereço fornecido está fora do mercado da loja.',
     'The amount of discount that is applied to the whole order. This amount is spread across line items in order of highest price to lowest price, until the discount is used up.' => 'O valor de desconto aplicado a todo o pedido. Esse valor é distribuído pelos itens de linha na ordem do preço mais alto para o preço mais baixo, até que o desconto esteja esgotado.',
     'The base discount can only discount items in the cart to down to zero until it is used up, it can not make the order negative.' => 'O desconto base só pode descontar itens do carrinho para zero até que seja gasto, e não pode tornar o pedido negativo.',
