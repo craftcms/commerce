ed42e62ec3eee14c12fa323839021dba5ff3b081
preciousdaleramirez <dalefirstpage@gmail.com>
2022-07-29 22:13:08 +0800

    change fixture key test

--- tests/unit/services/TaxCategoryTest.php
+++ tests/unit/services/TaxCategoryTest.php
@@ -33,7 +33,7 @@ class TaxCategoryTest extends Unit
     public function _fixtures(): array
     {
         return [
-            'products' => [
+            'products-tax' => [
                 'class' => ProductFixture::class,
             ],
         ];
