35aeb616c12d9f8195d4e6904f8ebfc758a2d738
Brad Bell <brad@pixelandtonic.com>
2022-11-25 19:47:12 -0800

    update ci

--- ecs.php
+++ ecs.php
@@ -1,21 +1,14 @@
 <?php
 
-declare(strict_types=1);
-
 use craft\ecs\SetList;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 
 return static function(ECSConfig $ecsConfig): void {
-    $ecsConfig->parallel();
     $ecsConfig->paths([
         __DIR__ . '/src',
-        __DIR__ . '/tests',
         __FILE__,
     ]);
 
-    $ecsConfig->skip([
-        __DIR__ . '/tests/_craft/storage',
-    ]);
-
+    $ecsConfig->parallel();
     $ecsConfig->sets([SetList::CRAFT_CMS_4]);
 };
