00b5e2e2e34384c7b3c3474dc3697f42634ef395
brandonkelly <brandon@pixelandtonic.com>
2022-11-29 11:50:00 -0800

    These columns should always exist

--- src/migrations/m221122_155735_update_orders_shippingMethodHandle_default.php
+++ src/migrations/m221122_155735_update_orders_shippingMethodHandle_default.php
@@ -14,19 +14,15 @@ class m221122_155735_update_orders_shippingMethodHandle_default extends Migratio
      */
     public function safeUp(): bool
     {
-        if ($this->db->getTableSchema()->getColumn('shippingMethodHandle')) {
-            $this->db->createCommand()
-                ->update('{{%commerce_orders}}', ['shippingMethodHandle' => ''], ['shippingMethodHandle' => null])
-                ->execute();
-            $this->alterColumn('{{%commerce_orders}}', 'shippingMethodHandle', $this->string()->notNull()->defaultValue(''));
-        }
+        $this->db->createCommand()
+            ->update('{{%commerce_orders}}', ['shippingMethodHandle' => ''], ['shippingMethodHandle' => null])
+            ->execute();
+        $this->alterColumn('{{%commerce_orders}}', 'shippingMethodHandle', $this->string()->notNull()->defaultValue(''));
 
-        if ($this->db->getTableSchema('{{%commerce_orders}}')->getColumn('shippingMethodName')) {
-            $this->db->createCommand()
-                ->update('{{%commerce_orders}}', ['shippingMethodName' => ''], ['shippingMethodName' => null])
-                ->execute();
-            $this->alterColumn('{{%commerce_orders}}', 'shippingMethodName', $this->string()->notNull()->defaultValue(''));
-        }
+        $this->db->createCommand()
+            ->update('{{%commerce_orders}}', ['shippingMethodName' => ''], ['shippingMethodName' => null])
+            ->execute();
+        $this->alterColumn('{{%commerce_orders}}', 'shippingMethodName', $this->string()->notNull()->defaultValue(''));
 
         return true;
     }
