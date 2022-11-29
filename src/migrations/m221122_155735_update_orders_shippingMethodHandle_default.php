b721b0c0439b2c365058bbaecca34417eac1eea5
brandonkelly <brandon@pixelandtonic.com>
2022-11-29 11:49:24 -0800

    Use $this->db

--- src/migrations/m221122_155735_update_orders_shippingMethodHandle_default.php
+++ src/migrations/m221122_155735_update_orders_shippingMethodHandle_default.php
@@ -14,15 +14,15 @@ class m221122_155735_update_orders_shippingMethodHandle_default extends Migratio
      */
     public function safeUp(): bool
     {
-        if ($this->getDb()->getTableSchema('{{%commerce_orders}}')->getColumn('shippingMethodHandle')) {
-            $this->getDb()->createCommand()
+        if ($this->db->getTableSchema()->getColumn('shippingMethodHandle')) {
+            $this->db->createCommand()
                 ->update('{{%commerce_orders}}', ['shippingMethodHandle' => ''], ['shippingMethodHandle' => null])
                 ->execute();
             $this->alterColumn('{{%commerce_orders}}', 'shippingMethodHandle', $this->string()->notNull()->defaultValue(''));
         }
 
-        if ($this->getDb()->getTableSchema('{{%commerce_orders}}')->getColumn('shippingMethodName')) {
-            $this->getDb()->createCommand()
+        if ($this->db->getTableSchema('{{%commerce_orders}}')->getColumn('shippingMethodName')) {
+            $this->db->createCommand()
                 ->update('{{%commerce_orders}}', ['shippingMethodName' => ''], ['shippingMethodName' => null])
                 ->execute();
             $this->alterColumn('{{%commerce_orders}}', 'shippingMethodName', $this->string()->notNull()->defaultValue(''));
