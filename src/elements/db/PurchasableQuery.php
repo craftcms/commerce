06c42fab5e91dec1d8881325e59e9db9b93c0a1a
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2022-11-29 17:00:41 +0000

    Create `PurchasableQuery`

---
+++ src/elements/db/PurchasableQuery.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * @link https://craftcms.com/
+ * @copyright Copyright (c) Pixel & Tonic, Inc.
+ * @license https://craftcms.github.io/license/
+ */
+
+namespace craft\commerce\elements\db;
+
+use craft\commerce\base\Purchasable;
+use craft\commerce\db\Table;
+use craft\commerce\Plugin;
+use craft\elements\db\ElementQuery;
+use yii\db\Connection;
+
+/**
+ * PurchasableQuery represents a SELECT SQL statement for purchasables in a way that is independent of DBMS.
+ *
+ * @method Purchasable[]|array all($db = null)
+ * @method Purchasable|array|null one($db = null)
+ * @method Purchasable|array|null nth(int $n, Connection $db = null)
+ * @since 5.0.0
+ */
+class PurchasableQuery extends ElementQuery
+{
+    protected array $defaultOrderBy = ['commerce_purchasables.sku' => SORT_ASC];
+
+    public ?int $storeId = null;
+
+    /**
+     * @param int|null $storeId
+     * @return int
+     */
+    public function storeId(?int $storeId = null): int
+    {
+        $this->storeId = $storeId;
+        return $this->storeId;
+    }
+
+    public function populate($rows): array
+    {
+        if (empty($rows)) {
+            return [];
+        }
+
+        $storeHandles = Plugin::getInstance()->getStores()->getAllStores()->map(fn($store) => $store->handle)->all();
+        foreach ($rows as &$row) {
+            $row['basePrices'] = [];
+            $row['baseSalePrices'] = [];
+            foreach ($storeHandles as $storeHandle) {
+                $row['basePrices'][$storeHandle] = $row[$storeHandle . '_basePrice'] ?? null;
+                $row['baseSalePrices'][$storeHandle] = $row[$storeHandle . '_baseSalePrice'] ?? null;
+                unset($row[$storeHandle . '_basePrice'], $row[$storeHandle . '_baseSalePrice']);
+            }
+        }
+
+        return parent::populate($rows);
+    }
+
+    protected function beforePrepare(): bool
+    {
+        $this->joinElementTable('commerce_purchasables');
+        $storeIdsByHandle = Plugin::getInstance()->getStores()->getAllStores()->keyBy('handle')->map(fn($store) => $store->id)->all();
+        $select = [];
+        $joins = [];
+        foreach ($storeIdsByHandle as $storeHandle => $storeId) {
+            if ($this->storeId !== null && $this->storeId !== $storeId) {
+                continue;
+            }
+
+            $tableName = "commerce_pricing_price_$storeHandle";
+            $saleTableName = "commerce_pricing_sale_price_$storeHandle";
+            $select[] = "$tableName.price as {$storeHandle}_basePrice";
+            $select[] = "$saleTableName.price as {$storeHandle}_baseSalePrice";
+            $joins[] = [
+                'table' => Table::CATALOG_PRICING . " $tableName",
+                'on' => "[[commerce_purchasables.id]] = [[$tableName.purchasableId]] AND [[$tableName.storeId]] =:{$storeHandle}PriceStoreId AND [[$tableName.catalogPricingRuleId]] is NULL  AND [[$tableName.isPromotionalPrice]] = 0",
+                'params' => [":{$storeHandle}PriceStoreId" => $storeId],
+            ];
+            $joins[] = [
+                'table' => Table::CATALOG_PRICING . " $saleTableName",
+                'on' => "[[commerce_purchasables.id]] = [[$saleTableName.purchasableId]] AND [[$saleTableName.storeId]] =:{$storeHandle}SalePriceStoreId AND [[$saleTableName.catalogPricingRuleId]] is NULL AND [[$saleTableName.isPromotionalPrice]] = 1",
+                'params' => [":{$storeHandle}SalePriceStoreId" => $storeId],
+            ];
+        }
+
+        if (!empty($select)) {
+            $this->query->addSelect($select);
+        }
+
+        foreach ($joins as $join) {
+            $this->query->leftJoin($join['table'], $join['on'], $join['params']);
+        }
+
+        return parent::beforePrepare();
+    }
+}
