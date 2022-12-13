0b723e901a36de4b9169c170bc76463a735096dc
Luke Holder <luke@pixelandtonic.com>
2022-12-13 16:36:06 +0800

    Cleanup

--- src/services/Stores.php
+++ src/services/Stores.php
@@ -431,26 +431,6 @@ class Stores extends Component
     }
 
     /**
-     * @param string $handle
-     * @return Store|null
-     */
-    public function getStoreByHandle(string $handle): ?Store
-    {
-        return $this->getAllStores()->firstWhere('handle', $handle);
-    }
-
-    /**
-     * Returns the current store for the active request.
-     *
-     * @return Store
-     */
-    public function getCurrentStore(): Store
-    {
-        // @TODO update this with actual logic
-        return $this->getPrimaryStore();
-    }
-
-    /**
      * Gets a store record by uid.
      *
      * @param string $uid
