624492bf2e8200f962710467bd2a71d0e3e5a95b
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2023-05-31 15:13:52 +0100

    Remove leftover code

--- src/Plugin.php
+++ src/Plugin.php
@@ -634,7 +634,6 @@ class Plugin extends BasePlugin
 
         Event::on(UserElement::class, UserElement::EVENT_BEFORE_DELETE, [$this->getSubscriptions(), 'beforeDeleteUserHandler']);
         Event::on(UserElement::class, UserElement::EVENT_BEFORE_DELETE, [$this->getOrders(), 'beforeDeleteUserHandler']);
-        Event::on(UserElement::class, UserElement::EVENT_AFTER_SAVE, [$this->getCustomers(), 'afterSaveUserHandler']);
 
         Event::on(
             UserElement::class,
