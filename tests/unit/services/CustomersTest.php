48844d12fb4e51cc69148dd5cef607c245663978
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2023-06-23 11:16:17 +0100

    add test for registering on checkout

--- tests/unit/services/CustomersTest.php
+++ tests/unit/services/CustomersTest.php
@@ -9,11 +9,16 @@ namespace unit\services;
 
 use Codeception\Test\Unit;
 use craft\commerce\elements\Order;
+use craft\commerce\errors\OrderStatusException;
 use craft\commerce\Plugin;
 use craft\commerce\services\Customers;
+use craft\elements\User;
+use craft\errors\ElementNotFoundException;
 use craftcommercetests\fixtures\CustomerFixture;
 use craftcommercetests\fixtures\OrdersFixture;
 use UnitTester;
+use yii\base\Exception;
+use yii\base\InvalidConfigException;
 
 /**
  * CustomersTest
@@ -83,8 +88,8 @@ class CustomersTest extends Unit
         ]));
 
         $order = new Order();
-        $email = 'test@newemailaddress.xyz';
-        $order->setEmail($email);
+        $user = \Craft::$app->getUsers()->ensureUserByEmail('test@newemailaddress.xyz');
+        $order->setCustomer($user);
 
         /** @var Order $order */
         $completedOrder = $this->fixtureData->getElement('completed-new');
@@ -97,6 +102,65 @@ class CustomersTest extends Unit
         self::assertTrue($order->markAsComplete());
 
         $this->_deleteElementIds[] = $order->id;
+        $this->_deleteElementIds[] = $user->id;
+    }
+
+    /**
+     * @param string $email
+     * @param bool $register
+     * @param bool $deleteUser an argument to help with cleanup
+     * @return void
+     * @throws \Throwable
+     * @throws OrderStatusException
+     * @throws ElementNotFoundException
+     * @throws Exception
+     * @throws InvalidConfigException
+     * @dataProvider registerOnCheckoutDataProvider
+     */
+    public function testRegisterOnCheckout(string $email, bool $register, bool $deleteUser): void
+    {
+        $order = new Order();
+        $user = \Craft::$app->getUsers()->ensureUserByEmail($email);
+        $originallyCredentialed = $user->getIsCredentialed();
+        $order->setCustomer($user);
+
+        $order->registerUserOnOrderComplete = $register;
+
+        $completedOrder = $this->fixtureData->getElement('completed-new');
+        $lineItem = $completedOrder->getLineItems()[0];
+        $qty = 4;
+        $note = 'My note';
+        $lineItem = Plugin::getInstance()->getLineItems()->createLineItem($order, $lineItem->purchasableId, [], $qty, $note);
+        $order->setLineItems([$lineItem]);
+
+        self::assertTrue($order->markAsComplete());
+
+        $foundUser = User::find()->email($email)->status(null)->one();
+        self::assertNotNull($foundUser);
+
+        if ($register || $originallyCredentialed) {
+            self::assertTrue($foundUser->getIsCredentialed());
+        } else {
+            self::assertFalse($foundUser->getIsCredentialed());
+        }
+
+        $this->_deleteElementIds[] = $order->id;
+        if ($deleteUser) {
+            $this->_deleteElementIds[] = $user->id;
+        }
+    }
+
+    /**
+     * @return array[]
+     */
+    public function registerOnCheckoutDataProvider(): array
+    {
+        return [
+            'dont-register-guest' => ['guest@customer.xyz', false, true],
+            'register-guest' => ['guest@customer.xyz', true, true],
+            'register-credentialed-user' => ['cred.user@crafttest.com', true, false],
+            'dont-register-credentialed-user' => ['cred.user@crafttest.com', false, false],
+        ];
     }
 
     /**
