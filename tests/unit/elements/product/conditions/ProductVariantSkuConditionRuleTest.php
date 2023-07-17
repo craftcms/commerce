b923c7744f183551d6de4ed8e9b9fae45baba55b
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2023-07-17 09:32:09 +0100

    Added product variant sku condtion rule test

---
+++ tests/unit/elements/product/conditions/ProductVariantSkuConditionRuleTest.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * @link https://craftcms.com/
+ * @copyright Copyright (c) Pixel & Tonic, Inc.
+ * @license https://craftcms.github.io/license/
+ */
+
+namespace unit\elements\product\conditions;
+
+use Codeception\Test\Unit;
+use craft\commerce\elements\conditions\products\ProductTypeConditionRule;
+use craft\commerce\elements\conditions\products\ProductVariantSkuConditionRule;
+use craft\commerce\elements\Product;
+use craftcommercetests\fixtures\ProductFixture;
+
+/**
+ * ProductVariantSkuConditionRuleTest
+ *
+ * @author Pixel & Tonic, Inc. <support@pixelandtonic.com>
+ * @since 4.3.0
+ */
+class ProductVariantSkuConditionRuleTest extends Unit
+{
+    /**
+     * @return array
+     */
+    public function _fixtures(): array
+    {
+        return [
+            'products' => [
+                'class' => ProductFixture::class,
+            ],
+        ];
+    }
+
+    /**
+     * @group Product
+     */
+    public function testMatchElement(): void
+    {
+        $condition = Product::createCondition();
+        /** @var ProductVariantSkuConditionRule $rule */
+        $rule = \Craft::$app->getConditions()->createConditionRule(ProductVariantSkuConditionRule::class);
+        $rule->value = 'rad-hood';
+        $condition->addConditionRule($rule);
+
+        $productsFixture = $this->tester->grabFixture('products');
+        /** @var Product $product */
+        $product = $productsFixture->getElement('rad-hoodie');
+
+        self::assertTrue($condition->matchElement($product));
+    }
+
+    /**
+     * @group Product
+     */
+    public function testNotMatchElement(): void
+    {
+        $condition = Product::createCondition();
+        /** @var ProductTypeConditionRule $rule */
+        $rule = \Craft::$app->getConditions()->createConditionRule(ProductVariantSkuConditionRule::class);
+        $rule->value = 'does-not-exist';
+        $condition->addConditionRule($rule);
+
+        $productsFixture = $this->tester->grabFixture('products');
+        /** @var Product $product */
+        $product = $productsFixture->getElement('rad-hoodie');
+
+        self::assertFalse($condition->matchElement($product));
+    }
+}
