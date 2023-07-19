b979371537e52551df93a0a33a2bb4e9c4e0b850
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2023-07-19 16:32:57 +0100

    Add `hasOrderCondtion` test

--- tests/unit/models/DiscountTest.php
+++ tests/unit/models/DiscountTest.php
@@ -8,7 +8,10 @@
 namespace craftcommercetests\unit\models;
 
 use Codeception\Test\Unit;
+use craft\commerce\elements\conditions\orders\OrderCondition;
 use craft\commerce\models\Discount;
+use craft\elements\conditions\ElementConditionInterface;
+use craft\elements\conditions\IdConditionRule;
 
 /**
  * DiscountTest
@@ -42,6 +45,44 @@ class DiscountTest extends Unit
             [-0.105, '10.5%'],
             [-0.10504, '10.504%'],
             ['-0.1050400', '10.504%'],
+        ];
+    }
+
+    /**
+     * @return void
+     * @since 4.3.0
+     * @dataProvider conditionBuilderDataProvider
+     */
+    public function testHasOrderCondition(ElementConditionInterface|array|string $orderCondition, bool $expected): void
+    {
+        if ($orderCondition === 'class' || $orderCondition === 'rules') {
+            /** @var OrderCondition $orderCondition */
+            $orderCondition = \Craft::$app->getConditions()->createCondition(OrderCondition::class);
+
+            if ($orderCondition === 'rules') {
+                $rule = \Craft::$app->getConditions()->createConditionRule([
+                    'type' => IdConditionRule::class,
+                    'value' => 1,
+                ]);
+                $orderCondition->addConditionRule($rule);
+            }
+        }
+
+        $discount = \Craft::createObject([
+            'class' => Discount::class,
+            'orderCondition' => $orderCondition,
+        ]);
+
+        self::assertSame($expected, $discount->hasOrderCondition());
+    }
+
+    public function conditionBuilderDataProvider(): array
+    {
+        return [
+            'blank-string' => ['', false],
+            'empty-array' => [[], false],
+            'no-rules' => ['class', false],
+            'rules' => ['rules', true],
         ];
     }
 }
