fc56e3df3b7ff234ae70d427dd677fd3a9f79efd
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2023-09-19 14:09:20 +0100

    CPR controller check to see if sales are in use

--- src/controllers/CatalogPricingController.php
+++ src/controllers/CatalogPricingController.php
@@ -21,6 +21,7 @@ use craft\models\Site;
 use craft\web\assets\htmx\HtmxAsset;
 use yii\base\InvalidArgumentException;
 use yii\web\BadRequestHttpException;
+use yii\web\ForbiddenHttpException;
 use yii\web\NotFoundHttpException;
 use yii\web\Response;
 use yii\base\InvalidConfigException;
@@ -40,6 +41,10 @@ class CatalogPricingController extends BaseStoreSettingsController
         }
 
         $this->requirePermission('commerce-managePromotions');
+
+        if (!Plugin::getInstance()->getCatalogPricingRules()->canUseCatalogPricingRules()) {
+            throw new ForbiddenHttpException('Unable to use catalog pricing rules while sales exist.');
+        }
 
         return true;
     }
