8f968c593750dcdaa481f37d2b15aad583ff0949
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2024-02-29 15:58:44 +0000

    Fix stock condition rule test

--- tests/unit/elements/product/conditions/ProductVariantStockConditionRuleTest.php
+++ tests/unit/elements/product/conditions/ProductVariantStockConditionRuleTest.php
@@ -8,8 +8,13 @@
 namespace unit\elements\product\conditions;
 
 use Codeception\Test\Unit;
+use craft\commerce\db\Table;
 use craft\commerce\elements\conditions\products\ProductVariantStockConditionRule;
 use craft\commerce\elements\Product;
+use craft\commerce\elements\Variant;
+use craft\commerce\elements\VariantCollection;
+use craft\commerce\Plugin;
+use craft\db\Query;
 use craftcommercetests\fixtures\ProductFixture;
 
 /**
@@ -45,15 +50,20 @@ class ProductVariantStockConditionRuleTest extends Unit
         $condition->addConditionRule($rule);
 
         $productsFixture = $this->tester->grabFixture('products');
-        /** @var Product $product */
-        $product = $productsFixture->getElement('rad-hoodie');
+        // /** @var Product $product */
+        // $product = $productsFixture->getElement('rad-hoodie');
 
-        $variants = $product->getVariants();
-        $variants->each(function(&$variant) {
-            $variant->stock = 9;
-            $variant->hasUnlimitedStock = false;
-        });
-        $product->setVariants($variants);
+
+        $product = $this->make(Product::class, [
+            'getVariants' => function() {
+                return VariantCollection::make([
+                    $this->make(Variant::class, [
+                        'getStock' => 9,
+                        'inventoryTracked' => true,
+                    ]),
+                ]);
+            }
+        ]);
 
         self::assertTrue($condition->matchElement($product));
     }
@@ -79,6 +89,7 @@ class ProductVariantStockConditionRuleTest extends Unit
 
     public function testModifyQueryMatch(): void
     {
+        $primaryStore = Plugin::getInstance()->getStores()->getPrimaryStore();
         $condition = Product::createCondition();
         /** @var ProductVariantStockConditionRule $rule */
         $rule = \Craft::$app->getConditions()->createConditionRule(ProductVariantStockConditionRule::class);
@@ -90,17 +101,25 @@ class ProductVariantStockConditionRuleTest extends Unit
         /** @var Product $product */
         $product = $productsFixture->getElement('rad-hoodie');
 
-        $variants = $product->getVariants();
-        $variants->each(function($variant) {
-            $variant->stock = 9;
-            $variant->hasUnlimitedStock = false;
+        $originalValues = (new Query())
+            ->from(Table::PURCHASABLES_STORES)
+            ->select(['purchasableId', 'stock', 'inventoryTracked'])
+            ->indexBy('purchasableId')
+            ->all();
 
-            \Craft::$app->getElements()->saveElement($variant, false, false, false, false);
-        });
+        \Craft::$app->getDb()->createCommand()
+            ->update(Table::PURCHASABLES_STORES, ['stock' => 9, 'inventoryTracked' => true], ['storeId' => $primaryStore->id])
+            ->execute();
 
         $query = Product::find();
         $condition->modifyQuery($query);
 
         self::assertContainsEquals($product->id, $query->ids());
+
+        foreach ($originalValues as $purchasableId => $values) {
+            \Craft::$app->getDb()->createCommand()
+                ->update(Table::PURCHASABLES_STORES, $values, ['purchasableId' => $purchasableId, 'storeId' => $primaryStore->id])
+                ->execute();
+        }
     }
 }
