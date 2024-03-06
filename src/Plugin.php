8799549d2377b2cf6a8e82e87061529ea32c7400
Luke Holder <lukemh@gmail.com>
2024-03-06 19:52:52 +0800

    Fixed #3379

--- src/Plugin.php
+++ src/Plugin.php
@@ -207,7 +207,7 @@ class Plugin extends BasePlugin
     /**
      * @inheritDoc
      */
-    public string $schemaVersion = '4.5.0';
+    public string $schemaVersion = '4.5.1';
 
     /**
      * @inheritdoc
--- src/records/Discount.php
+++ src/records/Discount.php
@@ -18,7 +18,9 @@ use yii\db\ActiveQueryInterface;
  * Discount record.
  *
  * @property bool $allCategories
+ * @property ?string $categoryIds
  * @property bool $allPurchasables
+ * @property ?string $purchasableIds
  * @property float $baseDiscount
  * @property float $purchaseTotal
  * @property string $baseDiscountType
