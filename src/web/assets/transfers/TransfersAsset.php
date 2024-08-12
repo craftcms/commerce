26eb5a32b5ab5cd28f05a437ae00ac5fe52f9d4d
Luke Holder <lukemh@gmail.com>
2024-08-12 21:57:29 +0800

    Not used

--- src/web/assets/transfers/TransfersAsset.php
+++ src/web/assets/transfers/TransfersAsset.php
@@ -31,7 +31,6 @@ class TransfersAsset extends AssetBundle
         $this->depends = [
             CpAsset::class,
             HtmxAsset::class,
-            AdminTableAsset::class,
         ];
 
         $this->css[] = 'css/transfers.css';
