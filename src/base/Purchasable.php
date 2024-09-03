74b11b791ff14b6796ba1bacd2f1979f8a0bc074
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2024-09-03 16:46:58 +0100

    Add `@var` doc for PHPStan

--- src/base/Purchasable.php
+++ src/base/Purchasable.php
@@ -987,6 +987,7 @@ abstract class Purchasable extends Element implements PurchasableInterface, HasS
             // added to inventory before it is saved as a permanent variant.
             if ($purchasableId) {
                 // Set the inventory item data
+                /** @var InventoryItem|null $inventoryItem */
                 $inventoryItem = InventoryItemRecord::find()->where(['purchasableId' => $purchasableId])->one();
                 if (!$inventoryItem) {
                     $inventoryItem = new InventoryItemRecord();
