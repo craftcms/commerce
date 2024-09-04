4e606b4c4284f279a6fcadff25b690286de2cfaa
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2024-09-04 09:24:37 +0100

    Update store trait to fallback to `siteId` if the class has it

--- src/base/StoreTrait.php
+++ src/base/StoreTrait.php
@@ -25,6 +25,19 @@ trait StoreTrait
      */
     public function getStore(): Store
     {
+        // If the store ID is not set check to see if the class has a `siteId` property and use that.
+        if ($this->storeId === null && !property_exists($this, 'storeId')) {
+            throw new InvalidConfigException('Store ID is required');
+        }
+
+        if ($this->storeId === null && property_exists($this, 'siteId')) {
+            $store = Plugin::getInstance()->getStores()->getStoreBySiteId($this->siteId);
+            if (!$store) {
+                throw new InvalidConfigException('Unable to locate store for site ID: ' . $this->siteId);
+            }
+            $this->storeId = $store->id;
+        }
+
         if (!$store = Plugin::getInstance()->getStores()->getStoreById($this->storeId)) {
             throw new InvalidConfigException('Invalid store ID: ' . $this->storeId);
         }
