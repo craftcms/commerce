f099b89c2e0dc8ac5a81bc2aed39ed63d9b4e5a4
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2025-03-18 09:48:30 +0000

    Fix

--- src/elements/Product.php
+++ src/elements/Product.php
@@ -1224,7 +1224,11 @@ class Product extends Element implements HasStoreInterface
         // Make sure each variant has an owner set in case of mass assignment of product and variants
         if (is_array($variants)) {
             foreach ($variants as &$variant) {
-                if (!isset($variant['owner'])) {
+                if ($variant instanceof Variant) {
+                    continue;
+                }
+
+                if (is_array($variant) && !isset($variant['owner'])) {
                     $variant = ['owner' => $this] + $variant;
                 }
             }
