32760a541b18afbe412bd6aad885d64e78406afe
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2025-07-21 14:12:14 +0100

    Double up on the gateways order condition #4077

--- src/Plugin.php
+++ src/Plugin.php
@@ -257,7 +257,7 @@ class Plugin extends BasePlugin
     /**
      * @inheritDoc
      */
-    public string $schemaVersion = '5.4.0.4';
+    public string $schemaVersion = '5.4.0.5';
 
     /**
      * @inheritdoc
