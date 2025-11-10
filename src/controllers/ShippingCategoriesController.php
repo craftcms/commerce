49448fe297cb5b540926ddf29425d5fd066c5ed6
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2025-11-10 08:14:32 +0000

    shipping categories as screen

--- src/controllers/ShippingCategoriesController.php
+++ src/controllers/ShippingCategoriesController.php
@@ -14,6 +14,8 @@ use craft\commerce\Plugin;
 use craft\helpers\ArrayHelper;
 use craft\helpers\Cp;
 use craft\helpers\Html;
+use craft\helpers\Json;
+use craft\web\View;
 use yii\base\Exception;
 use yii\base\InvalidConfigException;
 use yii\web\BadRequestHttpException;
@@ -61,12 +63,68 @@ class ShippingCategoriesController extends BaseShippingSettingsController
             ];
         }
 
-        return $this->renderTemplate('commerce/store-management/shipping/shippingcategories/index', [
-            'shippingCategories' => $shippingCategories,
-            'tableData' => $tableData,
-            'storeHandle' => $store->handle,
-            'store' => $store,
+        $this->getView()->registerTranslations('commerce', [
+            'Default',
+            'Description',
+            'Handle',
+            'Name',
+            'Yes',
         ]);
+
+        $tableData = Json::encode($tableData);
+
+        $js = <<<JS
+var columns = [
+        { name: 'chip', title: Craft.t('commerce', 'Name') },
+        { name: '__slot:handle', title: Craft.t('commerce', 'Handle') },
+        { name: 'description', title: Craft.t('commerce', 'Description') },
+        {
+            name: 'default',
+            title: Craft.t('commerce', 'Default'),
+            callback: function(value) {
+                if (value) {
+                    return '<div data-icon="check" title="'+Craft.escapeHtml(Craft.t('commerce','Yes'))+'"></div>';
+                }
+            }
+        },
+    ];
+
+    new Craft.VueAdminTable({
+        actions: [
+        {
+            label: '',
+            icon: 'settings',
+            actions: [
+                {
+                    label: Craft.t('commerce', 'Set Default Category'),
+                    action: 'commerce/shipping-categories/set-default-category',
+                    param: 'storeHandle',
+                    value: '{$storeHandle}',
+                    allowMultiple: false
+                }
+            ]
+        }
+    ],
+        checkboxes: true,
+        columns: columns,
+        container: '#shipping-vue-admin-table',
+        deleteAction: 'commerce/shipping-categories/delete',
+        padded: true,
+        tableData: {$tableData},
+    });
+    
+JS;
+
+
+        $this->getView()->registerJs($js, View::POS_END);
+
+        return $this->asStoreManagementCpScreen($storeHandle)
+            ->additionalButtonsHtml(Html::a(
+                Craft::t('commerce', 'New shipping category'),
+                $store->getStoreSettingsUrl('shippingcategories/new'),
+                ['class' => 'btn submit add icon']
+            ))
+            ->contentHtml(Html::tag('div', '', ['id' => 'shipping-vue-admin-table']));
     }
 
     /**
@@ -123,7 +181,21 @@ class ShippingCategoriesController extends BaseShippingSettingsController
         $allShippingCategories = Plugin::getInstance()->getShippingCategories()->getAllShippingCategories($store->id);
         $variables['isDefaultAndOnlyCategory'] = $variables['id'] && $allShippingCategories->count() === 1 && $allShippingCategories->firstWhere('id', $variables['id']);
 
-        return $this->renderTemplate('commerce/store-management/shipping/shippingcategories/_edit', $variables);
+        $metaSidebar = '';
+        if ($variables['shippingCategory']->id) {
+            $metaSidebar = Cp::metadataHtml([
+                Craft::t('app', 'Created at') => Craft::$app->getFormatter()->asDatetime($variables['shippingCategory']->dateCreated, 'short'),
+                Craft::t('app', 'Updated at') => Craft::$app->getFormatter()->asDatetime($variables['shippingCategory']->dateUpdated, 'short'),
+            ]);
+        }
+
+        return $this->asStoreManagementCpScreen($storeHandle, false)
+            ->title($variables['title'])
+            ->addCrumb(Craft::t('commerce', 'Shipping Categories'),$store->getStoreSettingsUrl('shippingcategories'))
+            ->action('commerce/shipping-categories/save')
+            ->redirectUrl($store->getStoreSettingsUrl('shippingcategories'))
+            ->metaSidebarHtml($metaSidebar)
+            ->contentTemplate('commerce/store-management/shipping/shippingcategories/_edit', $variables);
     }
 
     /**
