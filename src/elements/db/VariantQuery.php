f9e8f8c159165f9e555c2b66b362a6146faddcc4
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2025-11-26 15:22:07 +0000

    fix cs

--- src/elements/db/VariantQuery.php
+++ src/elements/db/VariantQuery.php
@@ -488,7 +488,7 @@ class VariantQuery extends PurchasableQuery
         // Forcing the use of the `sortOrder` index if no custom `orderBy` is set
         if ($sortOrderIndex !== null && empty($this->orderBy)) {
             $elementOwnersTable = Craft::$app->getDb()->schema->getRawTableName(\craft\db\Table::ELEMENTS_OWNERS);
-            $this->subQuery->innerJoin([new Expression('[['.$elementOwnersTable.']] AS elements_owners USE INDEX (' . $sortOrderIndex . ')')], $ownersCondition);
+            $this->subQuery->innerJoin([new Expression('[[' . $elementOwnersTable . ']] AS elements_owners USE INDEX (' . $sortOrderIndex . ')')], $ownersCondition);
         } else {
             $this->subQuery->innerJoin(['elements_owners' => CraftTable::ELEMENTS_OWNERS], $ownersCondition);
         }
