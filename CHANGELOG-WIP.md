c5b0a484f75e076a846bb725a69010938d9c3786
Luke Holder <lukemh@gmail.com>
2026-01-15 15:17:46 +0800

    Added `relatedToProducts` and `relatedToVariants` GraphQL query arguments.

--- CHANGELOG-WIP.md
+++ CHANGELOG-WIP.md
@@ -1,1 +1,3 @@
 # WIP Release notes for Commerce 5.6
+
+- Added `relatedToProducts` and `relatedToVariants` GraphQL query arguments, enabling queries for elements related to specific products or variants. ([#4202](https://github.com/craftcms/commerce/discussions/4202))
---
+++ src/gql/handlers/RelatedProducts.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @link https://craftcms.com/
+ * @copyright Copyright (c) Pixel & Tonic, Inc.
+ * @license https://craftcms.github.io/license/
+ */
+
+namespace craft\commerce\gql\handlers;
+
+use craft\commerce\elements\Product;
+use craft\gql\base\RelationArgumentHandler;
+
+/**
+ * Class RelatedProducts
+ *
+ * @author Pixel & Tonic, Inc. <support@pixelandtonic.com>
+ * @since 5.6.0
+ */
+class RelatedProducts extends RelationArgumentHandler
+{
+    protected string $argumentName = 'relatedToProducts';
+
+    /**
+     * @inheritdoc
+     */
+    protected function handleArgument($argumentValue): mixed
+    {
+        $argumentValue = parent::handleArgument($argumentValue);
+        return $this->getIds(Product::class, $argumentValue);
+    }
+}
